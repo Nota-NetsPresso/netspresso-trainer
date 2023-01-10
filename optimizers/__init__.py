07a3b23cda033a9a160828eee98263e5c86820a2
deepkyu <hyoungkyu.song@nota.ai>
2023-01-10 04:03:05 +0000

    update optimizers from timm

---
+++ optimizers/__init__.py
@@ -0,0 +1,15 @@
+from .adabelief import AdaBelief
+from .adafactor import Adafactor
+from .adamp import AdamP
+from .adamw import AdamW
+from .lamb import Lamb
+from .lars import Lars
+from .lookahead import Lookahead
+from .madgrad import MADGRAD
+from .nadam import Nadam
+from .nvnovograd import NvNovoGrad
+from .radam import RAdam
+from .ranger import Ranger
+from .rmsprop_tf import RMSpropTF
+from .sgdp import SGDP
+from .factory import create_optimizer, optimizer_kwargs
