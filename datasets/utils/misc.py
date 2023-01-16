1d7711693dc1a6e0e5c67d38323d68dd8bcc1e2f
deepkyu <hyoungkyu.song@nota.ai>
2023-01-16 05:35:13 +0000

    update pagination in datasets

---
+++ datasets/utils/misc.py
@@ -0,0 +1,10 @@
+
+
+def expand_to_chs(x, n):
+    if not isinstance(x, (tuple, list)):
+        x = tuple(repeat(x, n))
+    elif len(x) == 1:
+        x = x * n
+    else:
+        assert len(x) == n, 'normalization stats must match image channels'
+    return x
