8b4c7f1fdd0bf60c5f317d831d349e4104e5a944
deepkyu <hyoungkyu.song@nota.ai>
2023-04-24 07:47:51 +0000

    NPTAIO-31 fix checkpoint convert to ignore head

--- snippet_checkpoint_converter.py
+++ snippet_checkpoint_converter.py
@@ -7,9 +7,8 @@ from models.full.experimental.pidnet import pidnet
 from utils.pretrained_editor import convert_state_dict_to_model
 
 args = OmegaConf.load("config/pidnet.yaml")
-model = pidnet(args, num_classes=19)
+model = pidnet(args, num_classes=3)
 
-checkpoint_path = Path("pretrained") / "full" / "pidnet" / "PIDNet_S_ImageNet.pth.tar"
+checkpoint_path = Path("pretrained") / "full_backup" / "pidnet" / "PIDNet_S_ImageNet.pth.tar"
 state_dict = torch.load(str(checkpoint_path))['state_dict']
-# model.load_state_dict(state_dict)
 convert_state_dict_to_model('pidnet', model, state_dict)
--- utils/pretrained_editor.py
+++ utils/pretrained_editor.py
@@ -11,11 +11,18 @@ def convert_state_dict_to_model(model_name, model, state_dict):
     
     """pidnet
     def convert_string(x): return f"{x}"
+    def exclude_string(x: str):
+        if x.startswith("seghead_p"):
+            return True
+        if x.startswith("final_layer"):
+            return True
+        return False
     """
 
     """vanila
     """
     def convert_string(x): return f"{x}"
+    def exclude_string(x): return False
 
     print(list(model.state_dict().keys())[:5])
     print(list(state_dict.keys())[:5])
@@ -27,11 +34,13 @@ def convert_state_dict_to_model(model_name, model, state_dict):
             extracted_state_dict[layer_name] = state_dict[convert_string(layer_name)]
         else:
             no_match_layers.append(layer_name)
+            if exclude_string(layer_name):
+                continue    
             extracted_state_dict[layer_name] = value
 
     print(f"no_match_layers: \n{no_match_layers[:5]}")
     print(f"NO MATCH COUNT: {len(no_match_layers)}")
-    model.load_state_dict(extracted_state_dict)
+    model.load_state_dict(dict(extracted_state_dict), strict=False)
     _save_extracted_state_dict(extracted_state_dict, "result.pth")
 
     return model
