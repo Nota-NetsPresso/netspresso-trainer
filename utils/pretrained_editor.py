d6cd49504b18b5b579847217e37b98ab587072c9
deepkyu <hyoungkyu.song@nota.ai>
2023-04-24 07:26:25 +0000

    NPTAIO-31 PIDNet checkpoint converter setup

--- utils/pretrained_editor.py
+++ utils/pretrained_editor.py
@@ -5,7 +5,17 @@ UPDATE_PREFIX = "updated_"
 
 
 def convert_state_dict_to_model(model_name, model, state_dict):
+    """segformer
     def convert_string(x): return f"segformer.encoder.{x}"
+    """
+    
+    """pidnet
+    def convert_string(x): return f"{x}"
+    """
+
+    """vanila
+    """
+    def convert_string(x): return f"{x}"
 
     print(list(model.state_dict().keys())[:5])
     print(list(state_dict.keys())[:5])
@@ -20,6 +30,7 @@ def convert_state_dict_to_model(model_name, model, state_dict):
             extracted_state_dict[layer_name] = value
 
     print(f"no_match_layers: \n{no_match_layers[:5]}")
+    print(f"NO MATCH COUNT: {len(no_match_layers)}")
     model.load_state_dict(extracted_state_dict)
     _save_extracted_state_dict(extracted_state_dict, "result.pth")
 
