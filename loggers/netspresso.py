27678cedd6cefa8db6a498001a99ab23174f4b39
deepkyu <hyoungkyu.song@nota.ai>
2023-05-17 06:46:02 +0000

    NPTAIO-62 move netspresso api caller

--- loggers/netspresso.py
+++ loggers/netspresso.py
@@ -3,8 +3,7 @@ import requests
 
 from utils.logger import set_logger
 
-logger = set_logger('utils', level=os.getenv('LOG_LEVEL', default='INFO'))
-
+logger = set_logger('loggers', level=os.getenv('LOG_LEVEL', default='INFO'))
 
 DEPLOYMENT_MODE = os.environ.get("DEPLOYMENT_MODE")
 if DEPLOYMENT_MODE and DEPLOYMENT_MODE == "PROD":
@@ -16,12 +15,24 @@ STATUS_CODE_400 = 400
 
 
 class ModelSearchServerHandler:
-    def __init__(self, project_id: str, token: str):
+    def __init__(self, project_id: str, token: str, start_epoch: int=1):
         self.project_id = project_id
         self.token = token
         self.only_once = True
         self.is_sent = False
+        self.start_epoch = start_epoch
         self.dest = f"{MODELSEARCH_API_SERVER_BASE_URI}/project/{self.project_id}"
+        
+    def init_epoch(self):
+        self._epoch = 0
+        
+    @property
+    def epoch(self):
+        return self._epoch
+    
+    @epoch.setter
+    def epoch(self, value: int) -> None:
+        self._epoch = int(value)
 
     def report_elapsed_time_for_epoch(self, elapsed_time_one_epoch: int):
         if not (self.only_once and self.is_sent):
@@ -40,3 +51,8 @@ class ModelSearchServerHandler:
                 logger.error(e)
             finally:
                 self.is_sent = True
+
+    def __call__(self, elapsed_time: float):
+        if self._epoch == self.start_epoch:
+            elapsed_time_int = int(elapsed_time)
+            self.report_elapsed_time_for_epoch(elapsed_time_one_epoch=elapsed_time_int)
