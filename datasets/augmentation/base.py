af1d6aea08eba01d3ab924d05bbebe568d1ff3c1
Hyoung-Kyu Song <hyoungkyu.song@nota.ai>
2023-05-18 13:28:43 +0900

    Merge pull request #42 from nota-github/NPTAIO-74-aug-functionality-check
    NPTAIO-74-aug-functionality-check

---
+++ datasets/augmentation/base.py
@@ -0,0 +1,12 @@
+from typing import Any
+import cv2
+import albumentations as A
+
+class CV2BaseAug:
+    def __init__(self) -> None:
+        pass
+
+
+class AlbumentationsBaseAug:
+    def __init__(self) -> None:
+        pass
