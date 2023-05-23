49bd813ab06d95eadb6ecb608e0cd5c21d14509f
deepkyu <hyoungkyu.song@nota.ai>
2023-05-23 09:34:56 +0000

    NPTAIO-78 efficientformer weight converting

--- snippet_checkpoint_converter.py
+++ snippet_checkpoint_converter.py
@@ -3,11 +3,11 @@ from pathlib import Path
 from omegaconf import OmegaConf
 import torch
 
-from models.backbones.experimental.vit import vit
+from models.backbones.experimental.efficientformer import efficientformer
 from utils.pretrained_editor import convert_state_dict_to_model
 
-model = vit()
+model = efficientformer()
 
-checkpoint_path = Path("pretrained") / "backbones_backup" / "vit" / "vit-tiny.pt"
+checkpoint_path = Path("pretrained") / "backbones_backup" / "efficientformer" / "efficientformer_l1_1000d.pth"
 state_dict = torch.load(str(checkpoint_path))
-convert_state_dict_to_model('vit', model, state_dict)
+convert_state_dict_to_model('efficientformer', model, state_dict['model'])
