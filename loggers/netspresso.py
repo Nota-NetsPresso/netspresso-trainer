d9f81b81a8b19d87ef8d3052956c995785a7cb18
deepkyu <hyoungkyu.song@nota.ai>
2023-06-05 08:19:09 +0000

    NPTAIO-89 integration with custom mongodb

--- loggers/netspresso.py
+++ loggers/netspresso.py
@@ -1,27 +1,31 @@
 import os
 import requests
 
+from bson.objectid import ObjectId
+from pymongo.mongo_client import MongoClient
+
 from utils.logger import set_logger
 
 logger = set_logger('loggers', level=os.getenv('LOG_LEVEL', default='INFO'))
 
-DEPLOYMENT_MODE = os.environ.get("DEPLOYMENT_MODE")
-if DEPLOYMENT_MODE and DEPLOYMENT_MODE == "PROD":
-    MODELSEARCH_API_SERVER_BASE_URI = "https://searcher.netspresso.ai/api/v1"  # PROD mode
-else:
-    MODELSEARCH_API_SERVER_BASE_URI = "http://1.235.98.12:40001/api/v1"  # DEV mode
-
-STATUS_CODE_400 = 400
+MONGODB_TEMP_URI = "mongodb+srv://test-user:ptBcsFnAMs9nUAyC@traininglog0.tqiweaa.mongodb.net/?retryWrites=true&w=majority" 
 
 
 class ModelSearchServerHandler:
-    def __init__(self, project_id: str, token: str, start_epoch: int=1):
-        self.project_id = project_id
-        self.token = token
-        self.only_once = True
-        self.is_sent = False
-        self.start_epoch = start_epoch
-        self.dest = f"{MODELSEARCH_API_SERVER_BASE_URI}/project/{self.project_id}"
+    def __init__(self, task, model, mongodb_uri: str=MONGODB_TEMP_URI) -> None:
+        client = MongoClient(mongodb_uri)
+
+        try:
+            client.admin.command('ping')
+            logger.debug("Pinged your deployment. You successfully connected to MongoDB!")
+        except Exception as e:
+            raise e
+        
+        self._db = client['custom-training-board']['trainer-all-in-one']
+        self._session_id = None
+        
+        self._create_session(title=f"[{task}]{model}")
+    
         
     def init_epoch(self):
         self._epoch = 0
@@ -33,26 +37,50 @@ class ModelSearchServerHandler:
     @epoch.setter
     def epoch(self, value: int) -> None:
         self._epoch = int(value)
-
-    def report_elapsed_time_for_epoch(self, elapsed_time_one_epoch: int):
-        if not (self.only_once and self.is_sent):
-            try:
-                headers = {
-                    'Authorization': f"Bearer {self.token}",
-                }
-                response = requests.patch(self.dest, json={"elapsed_time": elapsed_time_one_epoch}, headers=headers)
-
-                logger.info(f"Status Code: {response.status_code}")
-                logger.debug(f"Content: {response.content}")
-
-                if response.status_code >= STATUS_CODE_400:
-                    raise Exception("To estimate elapsed time of one epoch failed.")
-            except Exception as e:
-                logger.error(e)
-            finally:
-                self.is_sent = True
-
-    def __call__(self, elapsed_time: float):
-        if self._epoch == self.start_epoch:
-            elapsed_time_int = int(elapsed_time)
-            self.report_elapsed_time_for_epoch(elapsed_time_one_epoch=elapsed_time_int)
+    
+    def _is_ready(self):
+        return self._session_id is not None
+        
+    def _append(self, scalar_dict, mode='train'):
+        assert self._is_ready()
+        meta_string = f"{mode}/" if mode is not None else ""
+        contents = {'$push': {f"{meta_string}{key}": {'epoch': self._epoch, 'value': value}
+                              for key, value in scalar_dict.items()},
+                    '$currentDate': {'lastModified': True }}
+        result = self._db.update_one({'_id': self._session_id}, contents, upsert=True)
+        return result
+        
+    def _create_session(self, title: str ="test") -> ObjectId:
+        example_document = { "title": title }
+        document = self._db.insert_one(example_document)
+        self._session_id = document.inserted_id
+        return self._session_id
+    
+    def create_session(self, title: str="test") -> ObjectId:
+        return self._create_session(title=title)
+    
+    def log_scalar(self, key, value, mode='train'):
+        result = self._append({key: value}, mode=mode)
+        return result
+        
+    def log_scalars_with_dict(self, scalar_dict, mode='train'):
+        result = self._append(scalar_dict, mode=mode)
+        return result
+    
+    def __call__(self,
+            train_losses, train_metrics, valid_losses, valid_metrics,
+            learning_rate, elapsed_time,
+        ) -> None:
+        
+        self.log_scalars_with_dict(train_losses, mode='train')
+        self.log_scalars_with_dict(train_metrics, mode='train')
+        
+        if valid_losses is not None:
+            self.log_scalars_with_dict(valid_losses, mode='valid')
+        if valid_metrics is not None:
+            self.log_scalars_with_dict(valid_metrics, mode='valid')
+        
+        if learning_rate is not None:
+            self.log_scalar('learning_rate', learning_rate, mode='misc')
+        if elapsed_time is not None:
+            self.log_scalar('elapsed_time', elapsed_time, mode='misc')
