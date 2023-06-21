7e6bfcfa2583078d727fd2f59db31741ba7256e3
deepkyu <hyoungkyu.song@nota.ai>
2023-06-21 07:22:58 +0000

    NPTAIO-125 update classification shell script

--- train_classification.sh
+++ train_classification.sh
@@ -1,3 +1,8 @@
+python train.py\
+  --data config/data/chess.yaml\
+  --config config/models/resnet.yaml\
+  --training config/training/resnet.yaml
+
 # python -m torch.distributed.launch
 #   --nproc_per_node 2\
 #   train.py\
@@ -5,7 +10,12 @@
 #   --config config/models/resnet.yaml\
 #   --training config/training/resnet.yaml
 
-python train.py\
-  --data config/data/chest-xray-classification.yaml\
-  --config config/models/resnet.yaml\
-  --training config/training/resnet.yaml
+#### HuggingFace datasets training
+# To use HuggingFace datasets, you need to additionally install requirements-data.txt
+# `pip install -r requirements-data.txt`
+#### (END)
+
+# python train.py\
+#   --data config/data/beans.yaml\
+#   --config config/models/resnet.yaml\
+#   --training config/training/resnet.yaml
