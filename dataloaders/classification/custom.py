7de9dc950899d13bffc86db62f2021fa7fc1043f
deepkyu <hyoungkyu.song@nota.ai>
2023-06-23 06:15:19 +0000

    NPTAIO-128 support train only dataset (classification only)

--- dataloaders/classification/custom.py
+++ dataloaders/classification/custom.py
@@ -1,6 +1,9 @@
 import os
+import csv
 from pathlib import Path
-from typing import Optional, Dict, Tuple
+from typing import Optional, Union, Tuple, List, Dict
+from collections import Counter
+from itertools import chain
 
 import PIL.Image as Image
 import torch
@@ -15,6 +18,7 @@ from utils.logger import set_logger
 
 logger = set_logger('data', level=os.getenv('LOG_LEVEL', default='INFO'))
 
+TRAIN_VALID_SPLIT_RATIO = 0.9
 _ERROR_RETRY = 50
 
 class ClassificationCustomDataset(BaseCustomDataset):
@@ -125,64 +129,101 @@ class ClassificationHFDataset(BaseHFDataset):
             target = -1
         return out['image'], target
     
-def load_custom_class_map(root, map_or_filename=None):
+def load_custom_class_map(root_dir, train_dir,
+                          map_or_filename: Optional[Union[str, Path]]=None,
+                          id_mapping: Optional[Dict[str, str]]=None):
 
-    dir_list = [x.name for x in Path(root).iterdir() if x.is_dir()]
+    if map_or_filename is None:  # may be labeled with directory
+        # dir -> 
+        dir_list = [x.name for x in Path(train_dir).iterdir() if x.is_dir()]
+        dir_to_class = id_mapping if id_mapping is not None else {k: k for k in dir_list}  # id_mapping or identity
+        
+        class_list = [dir_to_class[dir] for dir in dir_list]
+        class_list = sorted(class_list, key=lambda k: natural_key(k))
+        _class_to_idx = {class_name: class_idx for class_idx, class_name in enumerate(class_list)}
+        idx_to_class = {v: k for k, v in _class_to_idx.items()}
+        
+        file_or_dir_to_idx = {dir: _class_to_idx[dir_to_class[dir]] for dir in dir_list}  # dir -> idx
+        return file_or_dir_to_idx, idx_to_class
 
-    if map_or_filename is None:  # class_name == dir_name
-        dir_to_idx = {v.strip(): k for k, v in enumerate(dir_list)}
-        idx_to_class = {k: v.strip() for k, v in enumerate(dir_list)}
+    # Assume the `map_or_filename` is path for csv label file
+    class_map_path = Path(root_dir) / map_or_filename
+    assert class_map_path.exists(), f"Cannot locate specified class map file {class_map_path}!"
 
-    else:
-        if isinstance(map_or_filename, dict):
-            assert dict, "class_map dict must be non-empty"
-            dir_to_idx = {v.strip(): k for k, v in enumerate(map_or_filename.keys())}
-            idx_to_class = {k: v.strip() for k, v in enumerate(map_or_filename.values())}
-            return dir_to_idx, idx_to_class
+    class_map_ext = class_map_path.suffix.lower()
+    assert class_map_ext == '.csv', f"Unsupported class map file extension ({class_map_ext})!"
 
-        class_map_path = Path(map_or_filename)
-
-        if not class_map_path.exists():
-            class_map_path = Path(root) / class_map_path
-            assert class_map_path.exists(), f"Cannot locate specified class map file {map_or_filename}!"
-
-        class_map_ext = class_map_path.suffix.lower()
-        assert class_map_ext == '.txt', f"Unsupported class map file extension ({class_map_ext})!"
-
-        with open(class_map_path) as f:
-            map_data = [x.strip().split(' ') for x in f.readlines()]
-            dir_list_from_map = [x[0] for x in map_data]
-            assert set(dir_list).issubset(set(dir_list_from_map)), \
-                f"Found unknown directory in ({root}) whose class is not defined: {set(dir_list).difference(set(dir_list_from_map))}"
-            class_list_from_map = [' '.join(x[1:]) for x in map_data]
-            dir_to_idx = {v.strip(): k for k, v in enumerate(dir_list_from_map)}
-            idx_to_class = {k: v.strip() for k, v in enumerate(class_list_from_map)}
-
-    return dir_to_idx, idx_to_class
-
+    with open(class_map_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        file_class_list = [{column: str(row[column]).strip() for column in ['image_id', 'class']}
+                           for row in reader]
     
-def load_data(args_data: DictConfig, dir_to_idx, split='train'):
+    class_stats = Counter([x['class'] for x in file_class_list])
+    
+    _class_to_idx = {class_name: class_idx
+                    for class_idx, class_name in enumerate(sorted(class_stats, key=lambda k: natural_key(k)))}
+    idx_to_class = {v: k for k, v in _class_to_idx.items()}
+
+    file_or_dir_to_idx = {elem['image_id']: _class_to_idx[elem['class']] for elem in file_class_list}  # file -> idx
+
+    return file_or_dir_to_idx, idx_to_class
+
+def is_file_dict(image_dir: Union[Path, str], file_or_dir_to_idx):
+    image_dir = Path(image_dir)
+    candidate_name = list(file_or_dir_to_idx.keys())[0]
+    file_or_dir: Path = image_dir / candidate_name
+    if file_or_dir.exists():
+        return file_or_dir.is_file()
+    
+    file_candidates = [x for x in image_dir.glob(f"{candidate_name}.*")]
+    assert len(file_candidates) != 0, f"Unknown label format! Is there any something file like {file_or_dir} ?"
+    
+    return True
+    
+    
+def load_data(args_data: DictConfig, file_or_dir_to_idx, split='train'):
     data_root = Path(args_data.path.root)
     split_dir = args_data.path[split]
-    image_dir = data_root / split_dir.image
+    image_dir: Path = data_root / split_dir.image
     
-    images_and_targets = []
-    for dir_name, dir_idx in dir_to_idx.items():
-        _dir = Path(image_dir) / dir_name
-        for ext in IMG_EXTENSIONS:
-            images_and_targets.extend([{'image': str(file), 'label': dir_idx} for file in _dir.glob(f'*{ext}')])
-            images_and_targets.extend([{'image': str(file), 'label': dir_idx} for file in _dir.glob(f'*{ext.upper()}')])
+    images_and_targets: List[Dict[str, Optional[Union[str, int]]]] = []
+    if split in ['train', 'valid']:
+        
+        if is_file_dict(image_dir, file_or_dir_to_idx):
+            file_to_idx = file_or_dir_to_idx
+            for ext in IMG_EXTENSIONS:
+                for file in chain(image_dir.glob(f'*{ext}'), image_dir.glob(f'*{ext.upper()}')):
+                    if file.name in file_to_idx:
+                        images_and_targets.append({'image': str(file), 'label': file_to_idx[file.name]})
+                        continue
+                    if file.stem in file_to_idx:
+                        images_and_targets.append({'image': str(file), 'label': file_to_idx[file.stem]})
+                        continue
+                    logger.debug(f"Found file wihtout label: {file}")
+        
+        else:
+            dir_to_idx = file_or_dir_to_idx
+            for dir_name, dir_idx in dir_to_idx.items():
+                _dir = Path(image_dir) / dir_name
+                for ext in IMG_EXTENSIONS:
+                    images_and_targets.extend([{'image': str(file), 'label': dir_idx} for file in _dir.glob(f'*{ext}')])
+                    images_and_targets.extend([{'image': str(file), 'label': dir_idx} for file in _dir.glob(f'*{ext.upper()}')])
 
-    images_and_targets = sorted(images_and_targets, key=lambda k: natural_key(k['image']))
+        images_and_targets = sorted(images_and_targets, key=lambda k: natural_key(k['image']))
+    elif split == 'test':
+        for ext in IMG_EXTENSIONS:
+            images_and_targets.extend([{'image': str(file), 'label': None} for file in image_dir.glob(f'*{ext}')])
+            images_and_targets.extend([{'image': str(file), 'label': None} for file in image_dir.glob(f'*{ext.upper()}')])
 
     return images_and_targets
 
 
 def load_samples(args_data):
     assert args_data.path.train.image is not None
-    train_dir = Path(args_data.path.root) / args_data.path.train.image
-    class_map: Optional[dict] = dict(args_data.id_mapping) if args_data.id_mapping is not None else None
-    dir_to_idx, idx_to_class = load_custom_class_map(train_dir, class_map)
+    root_dir = Path(args_data.path.root)
+    train_dir = root_dir / args_data.path.train.image
+    id_mapping: Optional[dict] = dict(args_data.id_mapping) if args_data.id_mapping is not None else None
+    file_or_dir_to_idx, idx_to_class = load_custom_class_map(root_dir, train_dir, map_or_filename=args_data.path.train.label, id_mapping=id_mapping)
     
     exists_valid = args_data.path.valid.image is not None
     exists_test = args_data.path.test.image is not None
@@ -190,19 +231,19 @@ def load_samples(args_data):
     valid_samples = None
     test_samples = None
     
-    train_samples = load_data(args_data, dir_to_idx, split='train')
+    train_samples = load_data(args_data, file_or_dir_to_idx, split='train')
     if exists_valid:
-        valid_samples = load_data(args_data, dir_to_idx, split='valid')
+        valid_samples = load_data(args_data, file_or_dir_to_idx, split='valid')
     if exists_test:
-        test_samples = load_data(args_data, dir_to_idx, split='test')
+        test_samples = load_data(args_data, file_or_dir_to_idx, split='test')
 
-    if not exists_valid and not exists_test:
+    if not exists_valid:
+        num_train_splitted = int(len(train_samples) * TRAIN_VALID_SPLIT_RATIO) 
         train_samples, valid_samples = \
-            random_split(train_samples, [0.9, 0.1],
+            random_split(train_samples, [num_train_splitted, len(train_samples) - num_train_splitted],
                             generator=torch.Generator().manual_seed(42))
     
-    return train_samples, valid_samples, test_samples, \
-        {'dir_to_idx': dir_to_idx, 'idx_to_class': idx_to_class}
+    return train_samples, valid_samples, test_samples, {'idx_to_class': idx_to_class}
 
 def create_classification_dataset(args, transform, target_transform=None):
     data_format = args.data.format
