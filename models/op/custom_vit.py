ef55a03aaacc6a24e1dd0c6196488ef428503f5e
deepkyu <hyoungkyu.song@nota.ai>
2023-07-17 04:34:18 +0000

    NPTAIO-136 add hugging face vit

--- models/op/custom_vit.py
+++ models/op/custom_vit.py
@@ -60,16 +60,16 @@ class ViTEmbeddings(nn.Module):
     Construct the CLS token, position and patch embeddings. Optionally, also the mask token.
     """
 
-    def __init__(self, config, use_mask_token: bool = False) -> None:
+    def __init__(self, image_size, patch_size, in_channels, hidden_size, hidden_dropout_prob, use_mask_token: bool = False) -> None:
         super().__init__()
 
-        self.cls_token = nn.Parameter(torch.randn(1, 1, config.hidden_size))  # C(=hidden_size)
-        self.mask_token = nn.Parameter(torch.zeros(1, 1, config.hidden_size)) if use_mask_token else None  # Optional[C(=hidden_size)]
-        self.patch_embeddings = ViTPatchEmbeddings(config)
+        self.cls_token = nn.Parameter(torch.randn(1, 1, hidden_size))  # C(=hidden_size)
+        self.mask_token = nn.Parameter(torch.zeros(1, 1, hidden_size)) if use_mask_token else None  # Optional[C(=hidden_size)]
+        self.patch_embeddings = ViTPatchEmbeddings(image_size, patch_size, in_channels, hidden_size)
         num_patches = self.patch_embeddings.num_patches
-        self.position_embeddings = nn.Parameter(torch.randn(1, num_patches + 1, config.hidden_size))
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.config = config
+        self.position_embeddings = nn.Parameter(torch.randn(1, num_patches + 1, hidden_size))
+        self.dropout = nn.Dropout(hidden_dropout_prob)
+        self.patch_size = patch_size
 
     def interpolate_pos_encoding(self, embeddings: torch.Tensor, height: int, width: int) -> torch.Tensor:
         """
@@ -87,8 +87,8 @@ class ViTEmbeddings(nn.Module):
         class_pos_embed = self.position_embeddings[:, 0]
         patch_pos_embed = self.position_embeddings[:, 1:]
         dim = embeddings.shape[-1]
-        h0 = height // self.config.patch_size
-        w0 = width // self.config.patch_size
+        h0 = height // self.patch_size
+        w0 = width // self.patch_size
         # we add a small number to avoid floating point error in the interpolation
         # see discussion at https://github.com/facebookresearch/dino/issues/8
         h0, w0 = h0 + 0.1, w0 + 0.1
@@ -243,27 +243,262 @@ class MultiHeadSelfAttention(nn.Module):
         # return (context_layer,)
         return context_layer
     
-class ViTBlock(MetaFormerBlock):
-    def __init__(self, hidden_size, layer_norm_eps) -> None:
+    
+class ViTSelfOutput(nn.Module):
+    """
+    The residual connection is defined in ViTLayer instead of here (as is the case with other models), due to the
+    layernorm applied before each block.
+    """
+
+    def __init__(self, hidden_size, hidden_dropout_prob) -> None:
         super().__init__()
+        self.dense = nn.Linear(hidden_size, hidden_size)
+        self.dropout = nn.Dropout(hidden_dropout_prob)
+
+    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        hidden_states = self.dense(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+
+        return hidden_states
+    
+class ViTAttention(nn.Module):
+    def __init__(self, hidden_size, num_attention_heads, attention_scale, hidden_dropout_prob) -> None:
+        super().__init__()
+        self.attention = MultiHeadSelfAttention(        
+            hidden_size,
+            num_attention_heads,
+            attention_scale
+        )
+        self.output = ViTSelfOutput(hidden_size, hidden_dropout_prob)
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        head_mask: Optional[torch.Tensor] = None,
+        output_attentions: bool = False,
+    ) -> Union[Tuple[torch.Tensor, torch.Tensor], Tuple[torch.Tensor]]:
+        self_outputs = self.attention(hidden_states, head_mask, output_attentions)
+
+        attention_output = self.output(self_outputs[0], hidden_states)
+
+        outputs = (attention_output,) + self_outputs[1:]  # add attentions if we output them
+        return outputs
+    
+    
+class ViTIntermediate(nn.Module):
+    def __init__(self, hidden_size, intermediate_size) -> None:
+        super().__init__()
+        self.dense = nn.Linear(hidden_size, intermediate_size)
+        self.intermediate_act_fn = ACTIVATION_REGISTRY['gelu']()
+
+    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        hidden_states = self.dense(hidden_states)
+        hidden_states = self.intermediate_act_fn(hidden_states)
+
+        return hidden_states
+
+class ViTOutput(nn.Module):
+    def __init__(self, hidden_size, intermediate_size, hidden_dropout_prob) -> None:
+        super().__init__()
+        self.dense = nn.Linear(intermediate_size, hidden_size)
+        self.dropout = nn.Dropout(hidden_dropout_prob)
+
+    def forward(self, hidden_states: torch.Tensor, input_tensor: torch.Tensor) -> torch.Tensor:
+        hidden_states = self.dense(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+
+        hidden_states = hidden_states + input_tensor
+
+        return hidden_states
+
+
+class ViTLayer(nn.Module):
+    """This corresponds to the Block class in the timm implementation."""
+
+    def __init__(self, hidden_size, intermediate_size, num_attention_heads, attention_scale, layer_norm_eps, hidden_dropout_prob) -> None:
+        super().__init__()
+        self.seq_len_dim = 1
+        self.attention = ViTAttention(hidden_size, num_attention_heads, attention_scale, hidden_dropout_prob)
+        self.intermediate = ViTIntermediate(hidden_size, intermediate_size)
+        self.output = ViTOutput(hidden_size, intermediate_size, hidden_dropout_prob)
         self.layernorm_before = nn.LayerNorm(hidden_size, eps=layer_norm_eps)
         self.layernorm_after = nn.LayerNorm(hidden_size, eps=layer_norm_eps)
-        self.token_mixer = nn.Identity()  # TODO: define token mixer
-        self.channel_mlp = nn.Identity()  # TODO: define channel nlp
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        head_mask: Optional[torch.Tensor] = None,
+        output_attentions: bool = False,
+    ) -> Union[Tuple[torch.Tensor, torch.Tensor], Tuple[torch.Tensor]]:
+        self_attention_outputs = self.attention(
+            self.layernorm_before(hidden_states),  # in ViT, layernorm is applied before self-attention
+            head_mask,
+            output_attentions=output_attentions,
+        )
+        attention_output = self_attention_outputs[0]
+        outputs = self_attention_outputs[1:]  # add self attentions if we output attention weights
+
+        # first residual connection
+        hidden_states = attention_output + hidden_states
+
+        # in ViT, layernorm is also applied after self-attention
+        layer_output = self.layernorm_after(hidden_states)
+        layer_output = self.intermediate(layer_output)
+
+        # second residual connection is done here
+        layer_output = self.output(layer_output, hidden_states)
+
+        outputs = (layer_output,) + outputs
+
+        return outputs
+
+
+class ViTEncoder(nn.Module):
+    def __init__(self, num_hidden_layers, hidden_size, intermediate_size, num_attention_heads, attention_scale, layer_norm_eps, hidden_dropout_prob) -> None:
+        super().__init__()
+        self.layer = nn.ModuleList([ViTLayer(hidden_size, intermediate_size, num_attention_heads, attention_scale, layer_norm_eps, hidden_dropout_prob)
+                                    for _ in range(num_hidden_layers)])
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        head_mask: Optional[torch.Tensor] = None,
+        output_attentions: bool = False,
+        output_hidden_states: bool = False,
+        return_dict: bool = True,
+    ) -> Union[tuple, Dict]:
+        all_hidden_states = () if output_hidden_states else None
+        all_self_attentions = () if output_attentions else None
+
+        for i, layer_module in enumerate(self.layer):
+            if output_hidden_states:
+                all_hidden_states = all_hidden_states + (hidden_states,)
+
+            layer_head_mask = head_mask[i] if head_mask is not None else None
+
+            layer_outputs = layer_module(hidden_states, layer_head_mask, output_attentions)
+
+            hidden_states = layer_outputs[0]
+
+            if output_attentions:
+                all_self_attentions = all_self_attentions + (layer_outputs[1],)
+
+        if output_hidden_states:
+            all_hidden_states = all_hidden_states + (hidden_states,)
+
+        if not return_dict:
+            return tuple(v for v in [hidden_states, all_hidden_states, all_self_attentions] if v is not None)
+        return dict(
+            last_hidden_state=hidden_states,
+            hidden_states=all_hidden_states,
+            attentions=all_self_attentions,
+        )
+
+class ViTPooler(nn.Module):
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.dense = nn.Linear(hidden_size, hidden_size)
+        self.activation = nn.Tanh()
+
+    def forward(self, hidden_states):
+        # We "pool" the model by simply taking the hidden state corresponding
+        # to the first token.
+        first_token_tensor = hidden_states[:, 0]
+        pooled_output = self.dense(first_token_tensor)
+        pooled_output = self.activation(pooled_output)
+        return pooled_output
     
-    # def forward(self, x):
-    #     out_token_mixer = self.layernorm_before(x)
-    #     out_token_mixer = self.token_mixer(out_token_mixer)
-        
-    #     out_token_mixer = out_token_mixer + x
-        
-    #     out_final = self.layernorm_after(out_token_mixer)
-    #     out_final = self.channel_mlp(out_final)
-        
-    #     out_final = out_final + out_token_mixer
-        
-    #     return out_final
-        
+class ViTModel(nn.Module):
+    def __init__(
+        self,
+        num_hidden_layers,
+        image_size,
+        patch_size,
+        in_channels,
+        hidden_size,
+        intermediate_size,
+        num_attention_heads,
+        attention_scale,
+        layer_norm_eps,
+        hidden_dropout_prob,
+        add_pooling_layer: bool = True,
+        use_mask_token: bool = False
+    ):
+        super().__init__()
+
+        self.embeddings = ViTEmbeddings(image_size, patch_size, in_channels, hidden_size, hidden_dropout_prob, use_mask_token=use_mask_token)
+        self.encoder = ViTEncoder(num_hidden_layers, hidden_size, intermediate_size, num_attention_heads, attention_scale, layer_norm_eps, hidden_dropout_prob)
+
+        self.layernorm = nn.LayerNorm(hidden_size, eps=layer_norm_eps)
+        self.pooler = ViTPooler(hidden_size) if add_pooling_layer else None
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    def get_input_embeddings(self) -> ViTPatchEmbeddings:
+        return self.embeddings.patch_embeddings
+
+    def forward(
+        self,
+        pixel_values: Optional[torch.Tensor] = None,
+        bool_masked_pos: Optional[torch.BoolTensor] = None,
+        head_mask: Optional[torch.Tensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        interpolate_pos_encoding: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple, Dict]:
+        r"""
+        bool_masked_pos (`torch.BoolTensor` of shape `(batch_size, num_patches)`, *optional*):
+            Boolean masked positions. Indicates which patches are masked (1) and which aren't (0).
+        """
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        if pixel_values is None:
+            raise ValueError("You have to specify pixel_values")
+
+        # Prepare head mask if needed
+        # 1.0 in head_mask indicate we keep the head
+        # attention_probs has shape bsz x n_heads x N x N
+        # input head_mask has shape [num_heads] or [num_hidden_layers x num_heads]
+        # and head_mask is converted to shape [num_hidden_layers x batch x num_heads x seq_length x seq_length]
+        head_mask = self.get_head_mask(head_mask, self.config.num_hidden_layers)
+
+        # TODO: maybe have a cleaner way to cast the input (from `ImageProcessor` side?)
+        expected_dtype = self.embeddings.patch_embeddings.projection.weight.dtype
+        if pixel_values.dtype != expected_dtype:
+            pixel_values = pixel_values.to(expected_dtype)
+
+        embedding_output = self.embeddings(
+            pixel_values, bool_masked_pos=bool_masked_pos, interpolate_pos_encoding=interpolate_pos_encoding
+        )
+
+        encoder_outputs = self.encoder(
+            embedding_output,
+            head_mask=head_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+        sequence_output = encoder_outputs[0]
+        sequence_output = self.layernorm(sequence_output)
+        pooled_output = self.pooler(sequence_output) if self.pooler is not None else None
+
+        if not return_dict:
+            head_outputs = (sequence_output, pooled_output) if pooled_output is not None else (sequence_output,)
+            return head_outputs + encoder_outputs[1:]
+
+        return dict(
+            last_hidden_state=sequence_output,
+            pooler_output=pooled_output,
+            hidden_states=encoder_outputs.hidden_states,
+            attentions=encoder_outputs.attentions,
+        )
+
 
 class ViT(MetaFormer):
     def __init__(self, num_layers, hidden_size, layer_norm_eps) -> None:
@@ -288,3 +523,25 @@ class ViT(MetaFormer):
     #     x = self.forward_tokens(x)
     #     x = self.norm(x)
     #     return x
+    
+
+class ViTBlock(MetaFormerBlock):
+    def __init__(self, hidden_size, layer_norm_eps) -> None:
+        super().__init__()
+        self.layernorm_before = nn.LayerNorm(hidden_size, eps=layer_norm_eps)
+        self.layernorm_after = nn.LayerNorm(hidden_size, eps=layer_norm_eps)
+        self.token_mixer = nn.Identity()  # TODO: define token mixer
+        self.channel_mlp = nn.Identity()  # TODO: define channel nlp
+    
+    # def forward(self, x):
+    #     out_token_mixer = self.layernorm_before(x)
+    #     out_token_mixer = self.token_mixer(out_token_mixer)
+        
+    #     out_token_mixer = out_token_mixer + x
+        
+    #     out_final = self.layernorm_after(out_token_mixer)
+    #     out_final = self.channel_mlp(out_final)
+        
+    #     out_final = out_final + out_token_mixer
+        
+    #     return out_final
