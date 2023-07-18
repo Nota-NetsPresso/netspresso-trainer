be84ddc2a9982748824d232ee8d05c6796643645
deepkyu <hyoungkyu.song@nota.ai>
2023-07-18 13:14:55 +0000

    NPTAIO-136 weight porting for vit TODO: qkv weight split

--- snippet_checkpoint_converter.py
+++ snippet_checkpoint_converter.py
@@ -3,14 +3,14 @@ from pathlib import Path
 from omegaconf import OmegaConf
 import torch
 
-from models.full.experimental.pidnet import pidnet
+from models.backbones.experimental.vit import vit
 from utils.pretrained_editor import convert_state_dict_to_model
 
-yaml_path = Path("models/card") / "pidnet_s.yaml"
+yaml_path = Path("models/card") / "vit.yaml"
 
-model = pidnet(args=None, num_classes=10)
+model = vit(task='classification')
 
-checkpoint_path = Path("pretrained") / "full_backup" / "pidnet" / "PIDNet_S_ImageNet.pth.tar"
+checkpoint_path = Path("/CHECKPOINT") / "backbones_backup" / "vit" / "vit-tiny.pt"
 state_dict = torch.load(str(checkpoint_path))
 
 convert_state_dict_to_model(yaml_path,
--- utils/pretrained_editor.py
+++ utils/pretrained_editor.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from omegaconf import OmegaConf
+from omegaconf import OmegaConf, ListConfig
 import torch
 
 UPDATE_PREFIX = "updated_"
@@ -12,22 +12,26 @@ def convert_state_dict_to_model(yaml_path, model, state_dict):
     mapping = config.mapping
 
     model_state_dict = model.state_dict()
-    state_dict = state_dict[config.pretrained.state_dict_key]
+    if config.pretrained.state_dict_key is not None:
+        state_dict_key = config.pretrained.state_dict_key
+        state_dict = state_dict[state_dict_key]
 
     print(list(model_state_dict.keys())[:5])
     print(list(state_dict.keys())[:5])
 
     extracted_state_dict = {}
-    assert set(state_dict) == set(mapping)
     for layer_name, value in state_dict.items():
         assert layer_name in mapping, f"{layer_name} not in {yaml_path}"
         dest_layer_name = mapping[layer_name]
+
+        if isinstance(dest_layer_name, ListConfig):
+            dest_layer_name = None  # TODO: split qkv linear weights (and biases)
 
         if dest_layer_name is None:
             print(f"{layer_name} -> None. Skipped.")
             continue
 
-        assert dest_layer_name in model_state_dict, f"{dest_layer_name} not in model_state_dict"
+        assert dest_layer_name in model_state_dict, f"{dest_layer_name} ({type(dest_layer_name)}) not in model_state_dict"
         extracted_state_dict[dest_layer_name] = value
 
     no_match_layers = set(model_state_dict).difference(set(extracted_state_dict))
