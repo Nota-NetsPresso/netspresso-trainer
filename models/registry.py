fe7fc8f4bee4566853326289c72fb8feef0ef829
deepkyu <hyoungkyu.song@nota.ai>
2023-07-18 13:15:09 +0000

    NPTAIO-136 temp: change weight path

--- models/registry.py
+++ models/registry.py
@@ -17,7 +17,7 @@ MODEL_PRETRAINED_DICT = {
     "segformer": PRETRAINED_ROOT / "backbones" / "segformer" / "segformer.pth",
     "pidnet": PRETRAINED_ROOT / "full" / "pidnet" / "pidnet_s.pth",
     "mobilevit": PRETRAINED_ROOT / "backbones" / "mobilevit" / "mobilevit_s.pth",
-    "vit": PRETRAINED_ROOT / "backbones" / "vit" / "vit_tiny.pth",
+    "vit": PRETRAINED_ROOT / "backbones" / "vit" / "vit_without_qkv.pth",
     "efficientformer": PRETRAINED_ROOT / "backbones" / "efficientformer" / "efficientformer_l1_1000d.pth",
 }
 
