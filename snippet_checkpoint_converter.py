a25b84506338f482678e7d50288248392ed4ed11
deepkyu <hyoungkyu.song@nota.ai>
2023-07-26 11:47:04 +0000

    NPTAIO-136 add segformer model card with weight porting

--- snippet_checkpoint_converter.py
+++ snippet_checkpoint_converter.py
@@ -3,14 +3,14 @@ from pathlib import Path
 from omegaconf import OmegaConf
 import torch
 
-from models.backbones.experimental.mobilevit import mobilevit
+from models.backbones.experimental.segformer import segformer
 from utils.pretrained_editor import convert_state_dict_to_model
 
-yaml_path = Path("models/card") / "mobilevit.yaml"
+yaml_path = Path("models/card") / "segformer.yaml"
 
-model = mobilevit(task='classification')
+model = segformer(task='classification')
 
-checkpoint_path = Path("/CHECKPOINT") / "backbones_backup" / "mobilevit" / "mobilevit_s.pt"
+checkpoint_path = Path("/CHECKPOINT") / "backbones_backup" / "segformer" / "nvidia_mit-b0" / "pytorch_model.bin"
 state_dict = torch.load(str(checkpoint_path))
 
 convert_state_dict_to_model(yaml_path,
