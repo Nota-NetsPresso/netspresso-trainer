5a3f9c8fcdd6a7dede462fe30e80c557db14d47a
deepkyu <hyoungkyu.song@nota.ai>
2023-07-27 11:43:32 +0000

    NPTAIO-136 add efficientformer model card with weight porting

--- snippet_checkpoint_converter.py
+++ snippet_checkpoint_converter.py
@@ -3,14 +3,14 @@ from pathlib import Path
 from omegaconf import OmegaConf
 import torch
 
-from models.backbones.experimental.segformer import segformer
+from models.backbones.experimental.efficientformer import efficientformer
 from utils.pretrained_editor import convert_state_dict_to_model
 
-yaml_path = Path("models/card") / "segformer.yaml"
+yaml_path = Path("models/card") / "efficientformer.yaml"
 
-model = segformer(task='classification')
+model = efficientformer(task='classification')
 
-checkpoint_path = Path("/CHECKPOINT") / "backbones_backup" / "segformer" / "nvidia_mit-b0" / "pytorch_model.bin"
+checkpoint_path = Path("/CHECKPOINT") / "backbones_backup" / "efficientformer" / "efficientformer_l1_1000d.pth"
 state_dict = torch.load(str(checkpoint_path))
 
 convert_state_dict_to_model(yaml_path,
