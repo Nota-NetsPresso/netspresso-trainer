f376054fb1199d428b6e6f1676353f5f59f50b5c
deepkyu <hyoungkyu.song@nota.ai>
2023-07-28 02:01:52 +0000

    NPTAIO-143 weight porting for vit

--- snippet_checkpoint_converter.py
+++ snippet_checkpoint_converter.py
@@ -3,15 +3,17 @@ from pathlib import Path
 from omegaconf import OmegaConf
 import torch
 
-from models.backbones.experimental.efficientformer import efficientformer
+from models.backbones.experimental.vit import vit
 from utils.pretrained_editor import convert_state_dict_to_model
 
-yaml_path = Path("models/card") / "efficientformer.yaml"
+yaml_path = Path("models/card") / "vit.yaml"
 
-model = efficientformer(task='classification')
+model = vit(task='classification')
+# print(list(model.state_dict().keys()))
 
-checkpoint_path = Path("/CHECKPOINT") / "backbones_backup" / "efficientformer" / "efficientformer_l1_1000d.pth"
+checkpoint_path = Path("/CHECKPOINT") / "backbones_backup" / "vit" / "vit-tiny.pt"
 state_dict = torch.load(str(checkpoint_path))
+# print(list(state_dict.keys()))
 
 convert_state_dict_to_model(yaml_path,
                             model=model,
