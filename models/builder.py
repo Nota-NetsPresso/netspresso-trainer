71082d05a70b896e0ea77c92048fbae649b9ab63
deepkyu <hyoungkyu.song@nota.ai>
2023-07-28 06:48:37 +0000

    NPTAIO-145 task-specific model wrapper inheried from `TaskModel`

--- models/builder.py
+++ models/builder.py
@@ -1,83 +1,51 @@
 import os
-from typing import Type, Callable
+from typing import List, Dict, Type, Callable
 from pathlib import Path
 
 import torch
 import torch.nn as nn
 
+from models.base import TaskModel, ClassificationModel, SegmentationModel, DetectionModel, load_pretrained_checkpoint
 from models.registry import (
-    SUPPORTING_MODEL_LIST, MODEL_PRETRAINED_DICT, MODEL_BACKBONE_DICT, MODEL_FULL_DICT, MODEL_HEAD_DICT
+    SUPPORTING_MODEL_LIST, SUPPORTING_TASK_LIST,
+    MODEL_PRETRAINED_DICT, MODEL_BACKBONE_DICT, MODEL_FULL_DICT, MODEL_HEAD_DICT
 )
 from utils.logger import set_logger
 logger = set_logger('models', level=os.getenv('LOG_LEVEL', 'INFO'))
 
-UPDATE_PREFIX = "updated_"
+def load_full_model(args, model_name, num_classes):
+    model_fn: Callable[..., nn.Module] = MODEL_FULL_DICT[model_name]
+    model: nn.Module = model_fn(args, num_classes)
+
+    model_state_dict = load_pretrained_checkpoint(model_name)
+    missing_keys, unexpected_keys = model.load_state_dict(model_state_dict, strict=False)
+    logger.warning(f"Missing key(s) in state_dict: {missing_keys}")
+    logger.warning(f"Unexpected key(s) in state_dict: {unexpected_keys}")
+    return model
+
+def load_backbone_and_head_model(args, task, backbone_name, head_name, num_classes):
+    TASK_MODEL_DICT: Dict[str, Type[TaskModel]] = {
+        'classification': ClassificationModel,
+        'segmentation': SegmentationModel,
+        'detection': DetectionModel
+    }
+    
+    if not task in TASK_MODEL_DICT:
+        raise ValueError(f"No such task(s) named: {task}. This should be included in SUPPORTING_TASK_LIST ({SUPPORTING_TASK_LIST})")
+
+    return TASK_MODEL_DICT[task](args, task, backbone_name, head_name, num_classes)
 
 
-def load_pretrained_checkpoint(model_name: str):
-    assert model_name in SUPPORTING_MODEL_LIST, f"The model_name ({model_name}) should be one of the followings: {SUPPORTING_MODEL_LIST}"
-
-    if not model_name in MODEL_PRETRAINED_DICT:
-        raise AssertionError(f"No pretrained checkpoint found! Model name: ({model_name})")
-
-    state_dict_path: Path = MODEL_PRETRAINED_DICT[model_name]
-    state_dict = torch.load(state_dict_path)
-    return state_dict
-
-
-class AssembleModel(nn.Module):
-    def __init__(self, args, num_classes) -> None:
-        super(AssembleModel, self).__init__()
-        self.task = args.model.task
-        backbone_name = args.model.architecture.backbone
-        head = args.model.architecture.head
-        
-        backbone_fn: Callable[..., nn.Module] = MODEL_BACKBONE_DICT[backbone_name]
-        self.backbone = backbone_fn(task=self.task)
-        
-        try:
-            model_state_dict = load_pretrained_checkpoint(backbone_name)
-            missing_keys, unexpected_keys = self.backbone.load_state_dict(model_state_dict, strict=False)
-            logger.warning(f"Missing key(s) in state_dict: {missing_keys}")
-            logger.warning(f"Unexpected key(s) in state_dict: {unexpected_keys}")
-        except AssertionError as e:
-            logger.warning(str(e))
-        # self._freeze_backbone()
-        
-        head_module = MODEL_HEAD_DICT[self.task][head]
-        self.head = head_module(feature_dim=self.backbone.last_channels, num_classes=num_classes)
-        
-    def _freeze_backbone(self):
-        for m in self.backbone.parameters():
-            m.requires_grad = False
-
-    @property
-    def device(self):
-        return next(self.parameters()).device
-
-    def forward(self, x, label_size=None, targets=None):
-        features = self.backbone(x)
-        if self.task == 'classification':
-            out = self.head(features['last_feature'])
-        elif self.task == 'segmentation':
-            out = self.head(features['intermediate_features'], label_size=label_size)
-        elif self.task == 'detection':
-            out = self.head(features['intermediate_features'], targets=targets)
-
-        return out
-
-
-def build_model(args, num_classes):
+def build_model(args, num_classes) -> nn.Module:
+    
     if args.model.architecture.full is not None:
         model_name = args.model.architecture.full
-        model_fn: Callable[..., nn.Module] = MODEL_FULL_DICT[model_name]
-        model: nn.Module = model_fn(args, num_classes)
-
-        model_state_dict = load_pretrained_checkpoint(model_name)
-        missing_keys, unexpected_keys = model.load_state_dict(model_state_dict, strict=False)
-        logger.warning(f"Missing key(s) in state_dict: {missing_keys}")
-        logger.warning(f"Unexpected key(s) in state_dict: {unexpected_keys}")
-        return model
-
-    model = AssembleModel(args, num_classes)
-    return model
+        return load_full_model(args, model_name, num_classes)
+    
+    task = str(args.model.task).lower()
+    assert task in SUPPORTING_TASK_LIST
+    
+    backbone_name = str(args.model.architecture.backbone).lower()
+    head_name = str(args.model.architecture.head).lower()
+    
+    return load_backbone_and_head_model(args, task, backbone_name, head_name, num_classes)
