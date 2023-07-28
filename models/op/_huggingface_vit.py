03e7ab8e54386acda74c86d998ff419b741dc141
deepkyu <hyoungkyu.song@nota.ai>
2023-07-28 05:52:31 +0000

    NPTAIO-148 move norm and act registry in `op`

--- models/op/_huggingface_vit.py
+++ models/op/_huggingface_vit.py
@@ -9,7 +9,7 @@ from torch import Tensor
 from torch.fx.proxy import Proxy
 
 from models.op.base_metaformer import MultiHeadAttention
-from models.registry import NORM_REGISTRY, ACTIVATION_REGISTRY
+from models.op.registry import NORM_REGISTRY, ACTIVATION_REGISTRY
 
 
 class ViTPatchEmbeddings(nn.Module):
---
+++ models/op/registry.py
@@ -0,0 +1,18 @@
+from typing import Dict, Type
+from pathlib import Path
+import torch
+import torch.nn as nn
+
+NORM_REGISTRY: Dict[str, Type[nn.Module]] = {
+    'batch_norm': nn.BatchNorm2d,
+    'instance_norm': nn.InstanceNorm2d,
+}
+
+ACTIVATION_REGISTRY: Dict[str, Type[nn.Module]] = {
+    'relu': nn.ReLU,
+    'prelu': nn.PReLU,
+    'leaky_relu': nn.LeakyReLU,
+    'gelu': nn.GELU,
+    'silu': nn.SiLU,
+    'swish': nn.SiLU,
+}
