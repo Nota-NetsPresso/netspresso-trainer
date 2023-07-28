4211f3d3246a27e1832b91c107b69d46050e5f85
deepkyu <hyoungkyu.song@nota.ai>
2023-07-28 07:31:03 +0000

    NPTAIO-144 fix example fx converter

--- snippet_fx_convert.py
+++ snippet_fx_convert.py
@@ -1,11 +1,14 @@
+from pathlib import Path
+
+from omegaconf import OmegaConf
+
 from utils.fx import convert_graphmodule
+from models import build_model
+
 
 if __name__ == '__main__':
-    from models import AssembleModel
-    from omegaconf import OmegaConf
-
-    yaml_path = "config/resnet.yaml"
-    config = OmegaConf.load(yaml_path)
-    model = AssembleModel(config, num_classes=20)
+    model_yaml_path = Path("config") / "model" / "pidnet.yaml"
+    config = OmegaConf.load(model_yaml_path)
+    model = build_model(config, num_classes=20)
     fx_model = convert_graphmodule(model)
     print(fx_model)
