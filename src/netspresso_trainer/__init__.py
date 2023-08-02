7dec0bda63921f8b377da5d033ab3e75417f23b6
deepkyu <hyoungkyu.song@nota.ai>
2023-08-02 14:03:29 +0000

    NPTAIO-151 move core functionality into modules

--- src/netspresso_trainer/__init__.py
+++ src/netspresso_trainer/__init__.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from .train_common import train, set_arguments
+
 version = (Path(__file__).parent / "VERSION").read_text().strip()
 
 __version__ = version
--- train.py
+++ train.py
@@ -1,6 +1,4 @@
-import os
-
-from train_common import set_arguments, train
+from netspresso_trainer import set_arguments, train
 
 if __name__ == '__main__':
     args_parsed, args = set_arguments(is_graphmodule_training=False)
