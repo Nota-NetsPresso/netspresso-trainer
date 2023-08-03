ef583f0f0b52720622d0f6585a10ed65dc4ab904
deepkyu <hyoungkyu.song@nota.ai>
2023-08-03 16:32:41 +0000

    NPTAIO-51 remove unused functions

--- src/netspresso_trainer/optimizers/builder.py
+++ src/netspresso_trainer/optimizers/builder.py
@@ -3,155 +3,11 @@ Hacked together by / Copyright 2021 Ross Wightman
 """
 import json
 from itertools import islice
-from typing import Optional, Callable, Tuple
+from typing import Optional, Callable, Tuple, Literal
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
-
-def group_parameters(
-        module: nn.Module,
-        group_matcher,
-        output_values=False,
-        reverse=False,
-):
-    return group_with_matcher(
-        module.named_parameters(), group_matcher, output_values=output_values, reverse=reverse)
-
-def param_groups_weight_decay(
-        model: nn.Module,
-        weight_decay=1e-5,
-        no_weight_decay_list=()
-):
-    no_weight_decay_list = set(no_weight_decay_list)
-    decay = []
-    no_decay = []
-    for name, param in model.named_parameters():
-        if not param.requires_grad:
-            continue
-
-        if param.ndim <= 1 or name.endswith(".bias") or name in no_weight_decay_list:
-            no_decay.append(param)
-        else:
-            decay.append(param)
-
-    return [
-        {'params': no_decay, 'weight_decay': 0.},
-        {'params': decay, 'weight_decay': weight_decay}]
-
-
-def _group(it, size):
-    it = iter(it)
-    return iter(lambda: tuple(islice(it, size)), ())
-
-
-def _layer_map(model, layers_per_group=12, num_groups=None):
-    def _in_head(n, hp):
-        if not hp:
-            return True
-        elif isinstance(hp, (tuple, list)):
-            return any([n.startswith(hpi) for hpi in hp])
-        else:
-            return n.startswith(hp)
-
-    head_prefix = getattr(model, 'pretrained_cfg', {}).get('classifier', None)
-    names_trunk = []
-    names_head = []
-    for n, _ in model.named_parameters():
-        names_head.append(n) if _in_head(n, head_prefix) else names_trunk.append(n)
-
-    # group non-head layers
-    num_trunk_layers = len(names_trunk)
-    if num_groups is not None:
-        layers_per_group = -(num_trunk_layers // -num_groups)
-    names_trunk = list(_group(names_trunk, layers_per_group))
-
-    num_trunk_groups = len(names_trunk)
-    layer_map = {n: i for i, l in enumerate(names_trunk) for n in l}
-    layer_map.update({n: num_trunk_groups for n in names_head})
-    return layer_map
-
-
-def param_groups_layer_decay(
-        model: nn.Module,
-        weight_decay: float = 0.05,
-        no_weight_decay_list: Tuple[str] = (),
-        layer_decay: float = .75,
-        end_layer_decay: Optional[float] = None,
-):
-    """
-    Parameter groups for layer-wise lr decay & weight decay
-    Based on BEiT: https://github.com/microsoft/unilm/blob/master/beit/optim_factory.py#L58
-    """
-    no_weight_decay_list = set(no_weight_decay_list)
-    param_group_names = {}  # NOTE for debugging
-    param_groups = {}
-
-    if hasattr(model, 'group_matcher'):
-        # FIXME interface needs more work
-        layer_map = group_parameters(model, model.group_matcher(coarse=False), reverse=True)
-    else:
-        # fallback
-        layer_map = _layer_map(model)
-    num_layers = max(layer_map.values()) + 1
-    layer_max = num_layers - 1
-    layer_scales = list(layer_decay ** (layer_max - i) for i in range(num_layers))
-
-    for name, param in model.named_parameters():
-        if not param.requires_grad:
-            continue
-
-        # no decay: all 1D parameters and model specific ones
-        if param.ndim == 1 or name in no_weight_decay_list:
-            g_decay = "no_decay"
-            this_decay = 0.
-        else:
-            g_decay = "decay"
-            this_decay = weight_decay
-
-        layer_id = layer_map.get(name, layer_max)
-        group_name = "layer_%d_%s" % (layer_id, g_decay)
-
-        if group_name not in param_groups:
-            this_scale = layer_scales[layer_id]
-            param_group_names[group_name] = {
-                "lr_scale": this_scale,
-                "weight_decay": this_decay,
-                "param_names": [],
-            }
-            param_groups[group_name] = {
-                "lr_scale": this_scale,
-                "weight_decay": this_decay,
-                "params": [],
-            }
-
-        param_group_names[group_name]["param_names"].append(name)
-        param_groups[group_name]["params"].append(param)
-
-    # FIXME temporary output to debug new feature
-    print("parameter groups: \n%s" % json.dumps(param_group_names, indent=2))
-
-    return list(param_groups.values())
-
-
-def optimizer_kwargs(cfg):
-    """ cfg/argparse to kwargs helper
-    Convert optimizer args in argparse args or cfg like object to keyword args for updated create fn.
-    """
-    kwargs = dict(
-        opt=cfg.opt,
-        lr=cfg.lr,
-        wd=cfg.wd,
-        momentum=cfg.momentum)
-    if getattr(cfg, 'opt_eps', None) is not None:
-        kwargs['eps'] = cfg.opt_eps
-    if getattr(cfg, 'opt_betas', None) is not None:
-        kwargs['betas'] = cfg.opt_betas
-    if getattr(cfg, 'layer_decay', None) is not None:
-        kwargs['layer_decay'] = cfg.layer_decay
-    if getattr(cfg, 'opt_args', None) is not None:
-        kwargs.update(cfg.opt_args)
-    return kwargs
 
 
 def build_optimizer(
@@ -160,9 +16,6 @@ def build_optimizer(
         lr: Optional[float] = None,
         wd: float = 0.,
         momentum: float = 0.9,
-        filter_bias_and_bn: bool = True,
-        layer_decay: Optional[float] = None,
-        param_group_fn: Optional[Callable] = None,
         **kwargs):
     """ Create an optimizer.
 
@@ -180,31 +33,16 @@ def build_optimizer(
     """
     if isinstance(model_or_params, nn.Module):
         # a model was passed in, extract parameters and add weight decays to appropriate layers
-        no_weight_decay = {}
-        if hasattr(model_or_params, 'no_weight_decay'):
-            no_weight_decay = model_or_params.no_weight_decay()
-
-        if param_group_fn:
-            parameters = param_group_fn(model_or_params)
-        elif layer_decay is not None:
-            parameters = param_groups_layer_decay(
-                model_or_params,
-                weight_decay=wd,
-                layer_decay=layer_decay,
-                no_weight_decay_list=no_weight_decay)
-            wd = 0.
-        elif wd and filter_bias_and_bn:
-            parameters = param_groups_weight_decay(model_or_params, wd, no_weight_decay)
-            wd = 0.
-        else:
-            parameters = model_or_params.parameters()
+        parameters = model_or_params.parameters()
     else:
         # iterable of parameters or param groups passed in
         parameters = model_or_params
 
     opt_lower = opt.lower()
     opt_split = opt_lower.split('_')
-    opt_lower = opt_split[-1]
+    opt_lower: Literal['sgd', 'nesterov', 'momentum',
+                       'adam', 'adamw', 'adamax',
+                       'adadelta', 'adagrad', 'rmsprop'] = opt_split[-1]
 
     opt_args = dict(weight_decay=wd, **kwargs)
     if lr is not None:
