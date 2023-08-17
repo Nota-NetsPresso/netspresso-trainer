49881ac8d6d68eb6d321a4aa5deed146027d7f49
deepkyu <hyoungkyu.song@nota.ai>
2023-08-17 07:08:02 +0000

    Add cosine with warm restart (T_0 controlled by `iters_per_phase`) nota-github/netspresso-trainer-dev#113

--- src/netspresso_trainer/schedulers/registry.py
+++ src/netspresso_trainer/schedulers/registry.py
@@ -1,11 +1,16 @@
 from typing import Dict, Type
 
+import torch
 from torch.optim.lr_scheduler import _LRScheduler
 
+from .cosine_warm_restart import CosineAnnealingWarmRestartsWithCustomWarmUp
 from .cosine_lr import CosineAnnealingLRWithCustomWarmUp
 from .poly_lr import PolynomialLRWithWarmUp
+from .step_lr import StepLR
 
 SCHEDULER_DICT: Dict[str, Type[_LRScheduler]] = {
-    'cosine': CosineAnnealingLRWithCustomWarmUp,
-    'poly': PolynomialLRWithWarmUp
+    'cosine': CosineAnnealingWarmRestartsWithCustomWarmUp,
+    'cosine_no_sgdr': CosineAnnealingLRWithCustomWarmUp,
+    'poly': PolynomialLRWithWarmUp,
+    'step': StepLR
 }
