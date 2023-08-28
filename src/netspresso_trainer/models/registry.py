d2f99e552e37636616d980849c27e4f645a36e69
deepkyu <hyoungkyu.song@nota.ai>
2023-08-28 22:18:58 +0900

    update segformer head to use with generic configurations nota-github/netspresso-trainer-dev#121

--- src/netspresso_trainer/models/registry.py
+++ src/netspresso_trainer/models/registry.py
@@ -7,7 +7,7 @@ from .backbones import resnet50, segformer, mobilevit, vit, efficientformer
 from .full import pidnet
 
 from .heads.classification import fc
-from .heads.segmentation import segformer_decode_head, efficientformer_decode_head
+from .heads.segmentation import all_mlp_decoder
 from .heads.detection import efficientformer_detection_head
 
 MODEL_BACKBONE_DICT: Dict[str, Callable[..., nn.Module]] = {
@@ -23,8 +23,7 @@ MODEL_HEAD_DICT: Dict[str, Callable[..., nn.Module]] = {
         'fc': fc,
     },
     'segmentation': {
-        'segformer_decode_head': segformer_decode_head,
-        'efficientformer_decode_head': efficientformer_decode_head,
+        'all_mlp_decoder': all_mlp_decoder,
     },
     'detection': {
         'efficientformer_detection_head': efficientformer_detection_head
