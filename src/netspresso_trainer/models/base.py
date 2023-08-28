21f5e7c3f9b70dee84bb2f3d9840d1ed07fb9815
deepkyu <hyoungkyu.song@nota.ai>
2023-08-28 22:18:25 +0900

    apply different property per task nota-github/netspresso-trainer-dev#121

--- src/netspresso_trainer/models/base.py
+++ src/netspresso_trainer/models/base.py
@@ -19,20 +19,24 @@ class TaskModel(nn.Module):
         self.task = task
         self.backbone_name = backbone_name
         self.head_name = head_name
-        
+
         backbone_fn: Callable[..., nn.Module] = MODEL_BACKBONE_DICT[backbone_name]
         self.backbone: nn.Module = backbone_fn(task=self.task)
-        
+
         self.backbone = load_from_checkpoint(self.backbone, model_checkpoint)
-        
+
         head_module = MODEL_HEAD_DICT[self.task][head_name]
-        label_size = img_size if task in ['segmentation', 'detection'] else None
-        self.head = head_module(feature_dim=self.backbone.last_channels, num_classes=num_classes, label_size=label_size)
-        
+        if task == 'classification':
+            self.head = head_module(num_classes=num_classes, feature_dim=self.backbone.feature_dim)
+        elif task in ['segmentation', 'detection']:
+            self.head = head_module(num_classes=num_classes,
+                                    intermediate_features_dim=self.backbone.intermediate_features_dim,
+                                    label_size=img_size)
+
         if freeze_backbone:
             self._freeze_backbone()
             logger.info(f"Freeze! {self.backbone_name} is now freezed. Now only tuning with {self.head_name}.")
-        
+
     def _freeze_backbone(self):
         for m in self.backbone.parameters():
             m.requires_grad = False
@@ -40,10 +44,10 @@ class TaskModel(nn.Module):
     @property
     def device(self):
         return next(self.parameters()).device
-    
+
     def _get_name(self):
         return f"{self.__class__.__name__}[task={self.task}, backbone={self.backbone_name}, head={self.head_name}]"
-    
+
     @abstractmethod
     def forward(self, x, label_size=None, targets=None):
         raise NotImplementedError
@@ -53,7 +57,7 @@ class ClassificationModel(TaskModel):
     def __init__(self, conf_model, task, backbone_name, head_name, num_classes, model_checkpoint,
                  label_size=None, freeze_backbone=False) -> None:
         super().__init__(conf_model, task, backbone_name, head_name, num_classes, model_checkpoint, label_size, freeze_backbone)
-    
+
     def forward(self, x, label_size=None, targets=None):
         features: BackboneOutput = self.backbone(x)
         out: ModelOutput = self.head(features['last_feature'])
@@ -64,7 +68,7 @@ class SegmentationModel(TaskModel):
     def __init__(self, conf_model, task, backbone_name, head_name, num_classes, model_checkpoint,
                  label_size, freeze_backbone=False) -> None:
         super().__init__(conf_model, task, backbone_name, head_name, num_classes, model_checkpoint, label_size, freeze_backbone)
-    
+
     def forward(self, x, label_size=None, targets=None):
         features: BackboneOutput = self.backbone(x)
         out: ModelOutput = self.head(features['intermediate_features'])
@@ -75,9 +79,8 @@ class DetectionModel(TaskModel):
     def __init__(self, conf_model, task, backbone_name, head_name, num_classes, model_checkpoint,
                  label_size, freeze_backbone=False) -> None:
         super().__init__(conf_model, task, backbone_name, head_name, num_classes, model_checkpoint, label_size, freeze_backbone)
-    
+
     def forward(self, x, label_size=None, targets=None):
         features: BackboneOutput = self.backbone(x)
         out: DetectionModelOutput = self.head(features['intermediate_features'], targets=targets)
         return out
-    
