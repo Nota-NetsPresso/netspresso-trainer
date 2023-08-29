c13fa437aec03487ee7f17795acf862f2543eb8c
deepkyu <hyoungkyu.song@nota.ai>
2023-08-29 14:34:38 +0900

    update example command nota-github/netspresso-trainer-dev#123

--- example_train.sh
+++ example_train.sh
@@ -1,19 +1,102 @@
+#### HuggingFace datasets training
+# To use HuggingFace datasets, you need to additionally install requirements-data.txt
+# `pip install -r requirements-data.txt`
+#### (END)
+
+
+#### Classification
+
+netspresso-train\
+  --data config/data/beans.yaml\
+  --augmentation config/augmentation/classification.yaml\
+  --model config/model/resnet/resnet50-classification.yaml\
+  --training config/training/classification.yaml\
+  --logging config/logging.yaml\
+  --environment config/environment.yaml
+
 # netspresso-train\
-#   --data config/data/chess.yaml\
-#   --augmentation config/augmentation/resnet.yaml\
-#   --model config/model/resnet.yaml\
-#   --training config/training/resnet.yaml\
+#   --data config/data/beans.yaml\
+#   --augmentation config/augmentation/classification.yaml\
+#   --model config/model/efficientformer/efficientformer-l1-classification.yaml\
+#   --training config/training/classification.yaml\
 #   --logging config/logging.yaml\
 #   --environment config/environment.yaml
 
+# netspresso-train\
+#   --data config/data/beans.yaml\
+#   --augmentation config/augmentation/classification.yaml\
+#   --model config/model/mobilevit/mobilevit-s-classification.yaml\
+#   --training config/training/classification.yaml\
+#   --logging config/logging.yaml\
+#   --environment config/environment.yaml
+
+# netspresso-train\
+#   --data config/data/beans.yaml\
+#   --augmentation config/augmentation/classification.yaml\
+#   --model config/model/segformer/segformer-classification.yaml\
+#   --training config/training/classification.yaml\
+#   --logging config/logging.yaml\
+#   --environment config/environment.yaml
+
+# netspresso-train\
+#   --data config/data/beans.yaml\
+#   --augmentation config/augmentation/classification.yaml\
+#   --model config/model/vit/vit-classification.yaml\
+#   --training config/training/classification.yaml\
+#   --logging config/logging.yaml\
+#   --environment config/environment.yaml
+
+#### (END)
+
+
+#### Segmentation
+
+# netspresso-train\
+#   --data config/data/sidewalk-semantic.yaml\
+#   --augmentation config/augmentation/segmentation.yaml\
+#   --model config/model/efficientformer/efficientformer-l1-segmentation.yaml\
+#   --training config/training/segmentation.yaml\
+#   --logging config/logging.yaml\
+#   --environment config/environment.yaml
+
+# netspresso-train\
+#   --data config/data/sidewalk-semantic.yaml\
+#   --augmentation config/augmentation/segmentation.yaml\
+#   --model config/model/resnet50/resnet50-segmentation.yaml\
+#   --training config/training/segmentation.yaml\
+#   --logging config/logging.yaml\
+#   --environment config/environment.yaml
+
+# netspresso-train\
+#   --data config/data/sidewalk-semantic.yaml\
+#   --augmentation config/augmentation/segmentation.yaml\
+#   --model config/model/segformer/segformer-segmentation.yaml\
+#   --training config/training/segmentation.yaml\
+#   --logging config/logging.yaml\
+#   --environment config/environment.yaml
+
+# netspresso-train\
+#   --data config/data/sidewalk-semantic.yaml\
+#   --augmentation config/augmentation/segmentation.yaml\
+#   --model config/model/pidnet/pidnet-s-segmentation.yaml\
+#   --training config/training/segmentation.yaml\
+#   --logging config/logging.yaml\
+#   --environment config/environment.yaml
+
+#### (END)
+
+
+#### Detection
 
 # netspresso-train\
 #   --data config/data/traffic-sign.yaml\
-#   --augmentation config/augmentation/efficientformer-detection.yaml\
-#   --model config/model/efficientformer-detection.yaml\
-#   --training config/training/efficientformer.yaml\
+#   --augmentation config/augmentation/detection.yaml\
+#   --model config/model/efficientformer/efficientformer-l1-detection.yaml\
+#   --training config/training/detection.yaml\
 #   --logging config/logging.yaml\
 #   --environment config/environment.yaml
+
+#### (END)
 
 
 #### Multi-GPU training
@@ -24,9 +107,9 @@
 #   --nproc_per_node 4\
 #   train.py\
 #   --data config/data/chess.yaml\
-#   --augmentation config/augmentation/resnet.yaml\
-#   --model config/model/resnet.yaml\
-#   --training config/training/resnet.yaml\
+#   --augmentation config/augmentation/classification.yaml\
+#   --model config/model/resnet/resnet50-classification.yaml\
+#   --training config/training/classification.yaml\
 #   --logging config/logging.yaml\
 #   --environment config/environment.yaml
 
@@ -35,43 +118,19 @@
 #   --nproc_per_node 4\
 #   train.py\
 #   --data config/data/sidewalk-semantic.yaml\
-#   --augmentation config/augmentation/pidnet.yaml\
-#   --model config/model/pidnet.yaml\
-#   --training config/training/pidnet.yaml\
+#   --augmentation config/augmentation/segmentation.yaml\
+#   --model config/model/pidnet/pidnet-s-segmentation.yaml\
+#   --training config/training/segmentation.yaml\
 #   --logging config/logging.yaml\
-#   --environment config/environment.yaml\
-#   --log-level DEBUG
+#   --environment config/environment.yaml
 
 
 # python -m torch.distributed.launch\
 #   --nproc_per_node 4\
 #   train.py\
-#   --data config/data/coco_yolo.yaml\
-#   --augmentation config/augmentation/efficientformer-detection.yaml\
-#   --model config/model/efficientformer-detection.yaml\
-#   --training config/training/efficientformer.yaml\
+#   --data config/data/traffic-sign.yaml\
+#   --augmentation config/augmentation/detection.yaml\
+#   --model config/model/efficientformer/efficientformer-l1-detection.yaml\
+#   --training config/training/detection.yaml\
 #   --logging config/logging.yaml\
 #   --environment config/environment.yaml
-
-  
-#### HuggingFace datasets training
-# To use HuggingFace datasets, you need to additionally install requirements-data.txt
-# `pip install -r requirements-data.txt`
-#### (END)
-
-# netspresso-train\
-#   --data config/data/beans.yaml\
-#   --augmentation config/augmentation/vit.yaml\
-#   --model config/model/vit.yaml\
-#   --training config/training/vit.yaml\
-#   --logging config/logging.yaml\
-#   --environment config/environment.yaml\
-
-
-netspresso-train\
-  --data config/data/traffic-sign.yaml\
-  --augmentation config/augmentation/efficientformer-detection.yaml\
-  --model config/model/efficientformer-detection.yaml\
-  --training config/training/efficientformer.yaml\
-  --logging config/logging.yaml\
-  --environment config/environment.yaml
