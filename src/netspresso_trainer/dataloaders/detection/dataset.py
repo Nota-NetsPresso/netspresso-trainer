3b58b46b3ec03fab1de12ce7afa8771f8114ab65
deepkyu <hyoungkyu.song@nota.ai>
2023-09-11 11:45:45 +0900

    fix variable with class attribute nota-github/netspresso-trainer-dev#126

--- src/netspresso_trainer/dataloaders/detection/dataset.py
+++ src/netspresso_trainer/dataloaders/detection/dataset.py
@@ -101,7 +101,7 @@ class DetectionDataSampler(BaseDataSampler):
             test_samples = self.load_data(split='test')
 
         if not exists_valid:
-            num_train_splitted = int(len(train_samples) * TRAIN_VALID_SPLIT_RATIO) 
+            num_train_splitted = int(len(train_samples) * self.train_valid_split_ratio) 
             train_samples, valid_samples = \
                 random_split(train_samples, [num_train_splitted, len(train_samples) - num_train_splitted],
                                 generator=torch.Generator().manual_seed(42))
