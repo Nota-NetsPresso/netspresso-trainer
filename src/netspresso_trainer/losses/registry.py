805b6ea524944e30241a8d4c560a0f229c357aa5
deepkyu <hyoungkyu.song@nota.ai>
2023-09-11 12:19:13 +0900

    fix typo: BoundaryLoss nota-github/netspresso-trainer-dev#126

--- src/netspresso_trainer/losses/registry.py
+++ src/netspresso_trainer/losses/registry.py
@@ -1,15 +1,14 @@
+from .classification import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
 from .common import CrossEntropyLoss
-from .classification import LabelSmoothingCrossEntropy
-from .classification import SoftTargetCrossEntropy
-from .segmentation import PIDNetCrossEntropy, PIDNetBoundaryAwareCrossEntropy, BondaryLoss
 from .detection import RoiHeadLoss, RPNLoss
+from .segmentation import BoundaryLoss, PIDNetBoundaryAwareCrossEntropy, PIDNetCrossEntropy
 
 LOSS_DICT = {
     'cross_entropy': CrossEntropyLoss,
     'soft_target_cross_entropy': SoftTargetCrossEntropy,
     'label_smoothing_cross_entropy': LabelSmoothingCrossEntropy,
     'pidnet_cross_entropy': PIDNetCrossEntropy,
-    'boundary_loss': BondaryLoss,
+    'boundary_loss': BoundaryLoss,
     'pidnet_cross_entropy_with_boundary': PIDNetBoundaryAwareCrossEntropy,
     'roi_head_loss': RoiHeadLoss,
     'rpn_loss': RPNLoss,
