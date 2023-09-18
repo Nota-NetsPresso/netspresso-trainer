0749df7f3205f0a8331ec2979e43e977cd7bd665
deepkyu <hyoungkyu.song@nota.ai>
2023-09-18 18:05:09 +0900

    #150 support huggingface segmentation with class map

--- src/netspresso_trainer/dataloaders/segmentation/dataset.py
+++ src/netspresso_trainer/dataloaders/segmentation/dataset.py
@@ -114,9 +114,9 @@ class SegmentationDataSampler(BaseDataSampler):
             Path(cache_dir).mkdir(exist_ok=True, parents=True)
         total_dataset = load_dataset(root, name=subset_name, cache_dir=cache_dir)
 
-        assert self.conf_data.metadata.id_mapping is not None
-        id_mapping: Optional[list] = list(self.conf_data.metadata.id_mapping)
-        idx_to_class, label_value_to_idx = load_custom_class_map(id_mapping=id_mapping)
+        assert isinstance(self.conf_data.metadata.id_mapping, (ListConfig, DictConfig))
+
+        idx_to_class, label_value_to_idx = load_custom_class_map(id_mapping=self.conf_data.metadata.id_mapping)
 
         exists_valid = 'validation' in total_dataset
         exists_test = 'test' in total_dataset
