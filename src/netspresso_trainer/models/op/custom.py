60b38a1dc25d07ff440f0dc8513c126013775b7e
Junho Shin <junho.shin@nota.ai>
2023-10-16 15:47:29 +0900

    Add necessary modules to custom.py

--- src/netspresso_trainer/models/op/custom.py
+++ src/netspresso_trainer/models/op/custom.py
@@ -489,3 +489,142 @@ class GlobalPool(nn.Module):
 
     # def __repr__(self):
     #     return "{}(type={})".format(self.__class__.__name__, self.pool_type)
+
+
+class Focus(nn.Module):
+    """Focus width and height information into channel space."""
+
+    def __init__(self, in_channels, out_channels, ksize=1, stride=1, act_type="silu"):
+        super().__init__()
+        self.conv = ConvLayer(in_channels=in_channels * 4,
+                              out_channels=out_channels, 
+                              kernel_size=ksize, 
+                              stride=stride, 
+                              act_type=act_type)
+
+    def forward(self, x):
+        # shape of x (b,c,w,h) -> y(b,4c,w/2,h/2)
+        patch_top_left = x[..., ::2, ::2]
+        patch_top_right = x[..., ::2, 1::2]
+        patch_bot_left = x[..., 1::2, ::2]
+        patch_bot_right = x[..., 1::2, 1::2]
+        x = torch.cat(
+            (
+                patch_top_left,
+                patch_bot_left,
+                patch_top_right,
+                patch_bot_right,
+            ),
+            dim=1,
+        )
+        return self.conv(x)
+
+
+class CSPLayer(nn.Module):
+    """C3 in yolov5, CSP Bottleneck with 3 convolutions"""
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        n=1,
+        shortcut=True,
+        expansion=0.5,
+        #depthwise=False,
+        act_type="silu",
+    ):
+        """
+        Args:
+            in_channels (int): input channels.
+            out_channels (int): output channels.
+            n (int): number of Bottlenecks. Default value: 1.
+        """
+        # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        hidden_channels = int(out_channels * expansion)  # hidden channels
+        self.conv1 = ConvLayer(in_channels=in_channels, 
+                               out_channels=hidden_channels,
+                               kernel_size=1, 
+                               stride=1, act_type=act_type)
+        self.conv2 = ConvLayer(in_channels=in_channels,
+                              out_channels=hidden_channels, 
+                              kernel_size=1, 
+                              stride=1, act_type=act_type)
+        self.conv3 = ConvLayer(in_channels=2 * hidden_channels, 
+                               out_channels=out_channels, 
+                               kernel_size=1, 
+                               stride=1, act_type=act_type)
+        
+        # Newly define because of slight difference with Bottleneck of custom.py
+        class DarknetBlock(nn.Module):
+            # Standard bottleneck
+            def __init__(
+                self,
+                in_channels,
+                out_channels,
+                shortcut=True,
+                expansion=0.5,
+                #depthwise=False,
+                act_type="silu",
+            ):
+                super().__init__()
+                hidden_channels = int(out_channels * expansion)
+                self.conv1 = ConvLayer(in_channels=in_channels, out_channels=hidden_channels, 
+                                       kernel_size=1, stride=1, act_type=act_type)
+                self.conv2 = ConvLayer(in_channels=hidden_channels, out_channels=out_channels, 
+                                       kernel_size=3, stride=1, act_type=act_type)
+                self.use_add = shortcut and in_channels == out_channels
+
+            def forward(self, x):
+                y = self.conv2(self.conv1(x))
+                if self.use_add:
+                    y = y + x
+                return y
+            
+        block = DarknetBlock
+
+        module_list = [
+            block(
+                in_channels=hidden_channels, 
+                out_channels=hidden_channels, 
+                shortcut=shortcut,
+                expansion=1.0,
+                act_type=act_type
+            )
+            for _ in range(n)
+        ]
+        self.m = nn.Sequential(*module_list)
+
+    def forward(self, x):
+        x_1 = self.conv1(x)
+        x_2 = self.conv2(x)
+        x_1 = self.m(x_1)
+        x = torch.cat((x_1, x_2), dim=1)
+        return self.conv3(x)
+
+
+class SPPBottleneck(nn.Module):
+    """Spatial pyramid pooling layer used in YOLOv3-SPP"""
+
+    def __init__(
+        self, in_channels, out_channels, kernel_sizes=(5, 9, 13), act_type="silu"
+    ):
+        super().__init__()
+        hidden_channels = in_channels // 2
+        self.conv1 = ConvLayer(in_channels=in_channels, out_channels=hidden_channels, 
+                               kernel_size=1, stride=1, act_type=act_type)
+        self.m = nn.ModuleList(
+            [
+                nn.MaxPool2d(kernel_size=ks, stride=1, padding=ks // 2)
+                for ks in kernel_sizes
+            ]
+        )
+        conv2_channels = hidden_channels * (len(kernel_sizes) + 1)
+        self.conv2 = ConvLayer(in_channels=conv2_channels, out_channels=out_channels, 
+                               kernel_size=1, stride=1, act_type=act_type)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = torch.cat([x] + [m(x) for m in self.m], dim=1)
+        x = self.conv2(x)
+        return x
