fea91f921c4076d1e9a284bad7def604d8c1eb53
Junho Shin <junho.shin@nota.ai>
2023-10-20 17:47:08 +0900

    Change mobilenetv3 init method

--- src/netspresso_trainer/models/backbones/experimental/mobilenetv3.py
+++ src/netspresso_trainer/models/backbones/experimental/mobilenetv3.py
@@ -30,53 +30,46 @@ class MobileNetV3(nn.Module):
     def __init__(
         self,
         task: str,
-        block_info, # [in_channels, kernel, expended_channels, out_channels, use_se, activation, stride, dilation]
+        stages_info, # [in_channels, kernel, expended_channels, out_channels, use_se, activation, stride, dilation]
         **kwargs
     ) -> None:
         super(MobileNetV3, self).__init__()
 
         self.task = task.lower()
-        block = InvertedResidual
         self.use_intermediate_features = self.task in ['segmentation', 'detection']
         norm_type = 'batch_norm'
         act_type = 'hard_swish'
 
-        layers: List[nn.Module] = []
-
-        # block_info can have 2-level or 1-level hierarchy
-        if list_depth(block_info) == 3:
-            self.intermediate_out_step = list(accumulate([len(b)for b in block_info]))
-            self._intermediate_features_dim = [b[-1][3] for b in block_info]
-            block_info = sum(block_info, [])
-        else:
-            self.intermediate_out_step = [len(block_info)]
-            self._intermediate_features_dim = [block_info[-1][3]]
-        self.intermediate_out_step[-1] += 1 # last conv layer
-
         # building first layer
-        firstconv_output_channels = block_info[0][0]
-        layers.append(
-            ConvLayer(
-                in_channels=3,
-                out_channels=firstconv_output_channels,
-                kernel_size=3,
-                stride=2,
-                norm_type=norm_type,
-                act_type=act_type,
-            )
+        firstconv_output_channels = stages_info[0][0][0]
+        self.conv_first = ConvLayer(
+            in_channels=3,
+            out_channels=firstconv_output_channels,
+            kernel_size=3,
+            stride=2,
+            norm_type=norm_type,
+            act_type=act_type,
         )
 
         # building inverted residual blocks
-        for cnf in block_info:
-            in_channels = cnf[0]
-            kernel_size = cnf[1]
-            hidden_channels = cnf[2]
-            out_channels = cnf[3]
-            use_se = cnf[4]
-            act_type_b = cnf[5].lower()
-            stride = cnf[6]
-            dilation = cnf[7]
-            layers.append(block(in_channels=in_channels,
+        stages: List[nn.Module] = []
+
+        lastconv_input_channels = stages_info[-1][-1][3]
+        lastconv_output_channels = 6 * lastconv_input_channels
+        for stg_idx, stage_info in enumerate(stages_info):
+            stage: List[nn.Module] = []
+
+            for block in stage_info:
+                in_channels = block[0]
+                kernel_size = block[1]
+                hidden_channels = block[2]
+                out_channels = block[3]
+                use_se = block[4]
+                act_type_b = block[5].lower()
+                stride = block[6]
+                dilation = block[7]
+                stage.append(
+                    InvertedResidual(in_channels=in_channels,
                                      hidden_channels=hidden_channels,
                                      out_channels=out_channels,
                                      kernel_size=kernel_size,
@@ -84,30 +77,30 @@ class MobileNetV3(nn.Module):
                                      norm_type=norm_type,
                                      act_type=act_type_b,
                                      use_se=use_se,
-                                     dilation=dilation))
+                                     dilation=dilation)
+                )
+            
+            # add last conv
+            if stg_idx == len(stages_info) - 1:
+                stage.append(
+                    ConvLayer(in_channels=lastconv_input_channels,
+                              out_channels=lastconv_output_channels,
+                              kernel_size=1,
+                              norm_type=norm_type,
+                              act_type=act_type,)
+                )
 
-        # building last several layers
-        lastconv_input_channels = block_info[-1][3]
-        lastconv_output_channels = 6 * lastconv_input_channels
-        layers.append(
-            ConvLayer(
-                in_channels=lastconv_input_channels,
-                out_channels=lastconv_output_channels,
-                kernel_size=1,
-                norm_type=norm_type,
-                act_type=act_type,
-            )
-        )
-
-        self.features = nn.Sequential(*layers)
+            stage = nn.Sequential(*stage)
+            stages.append(stage)
+        
+        self.stages = nn.ModuleList(stages)
         self.avgpool = nn.AdaptiveAvgPool2d(1)
 
         self._feature_dim = lastconv_output_channels
-        self._intermediate_features_dim[-1] = lastconv_output_channels
-        if self.use_intermediate_features:
-            for i in self.intermediate_out_step:
-                self.features[i].register_forward_hook(self.get_intermediate_features())
+        self._intermediate_features_dim = [s[-1].out_channels for s in self.stages[:-1]]
+        self._intermediate_features_dim += [lastconv_output_channels]
 
+        # init weights
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode="fan_out")
@@ -120,19 +113,15 @@ class MobileNetV3(nn.Module):
                 nn.init.normal_(m.weight, 0, 0.01)
                 nn.init.zeros_(m.bias)
 
-    def get_intermediate_features(self):
-        def hook(model, input, output):
-            model.all_hidden_states.append(output)
-        return hook
-
     def forward(self, x: Tensor):
-        all_hidden_states = [] if self.use_intermediate_features else None
-        if self.use_intermediate_features:
-            for i in self.intermediate_out_step:
-                self.features[i].all_hidden_states = all_hidden_states
-        
-        x = self.features(x)
+        x = self.conv_first(x)
 
+        all_hidden_states = () if self.use_intermediate_features else None
+        for stage in self.stages:
+            x = stage(x)
+            if self.use_intermediate_features:
+                all_hidden_states = all_hidden_states + (x, )
+        
         if self.use_intermediate_features:
             return BackboneOutput(intermediate_features=all_hidden_states)
 
