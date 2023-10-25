ba56c55856d1ab01885f26cc0b70467820b599c8
deepkyu <hyoungkyu.song@nota.ai>
2023-10-25 19:51:02 +0900

    #203 update default value, export classes

--- src/netspresso_trainer/cfg/__init__.py
+++ src/netspresso_trainer/cfg/__init__.py
@@ -1,4 +1,17 @@
+from dataclasses import dataclass, field
 
-
+from .augmentation import (
+    AugmentationConfig,
+    ClassificationAugmentationConfig,
+    SegmentationAugmentationConfig,
+    DetectionAugmentationConfig,
+    ColorJitter
+)
 from .environment import EnvironmentConfig
 from .logging import LoggingConfig
+
+@dataclass
+class TrainerConfig:
+    augmentation: AugmentationConfig = field(default_factory=lambda: AugmentationConfig())
+    environment: EnvironmentConfig = field(default_factory=lambda: EnvironmentConfig())
+    logging: LoggingConfig = field(default_factory=lambda: LoggingConfig())
--- src/netspresso_trainer/cfg/augmentation.py
+++ src/netspresso_trainer/cfg/augmentation.py
@@ -20,20 +20,13 @@ class AugmentationConfig:
     min_scale: Optional[int] = None
     crop_size_h: Optional[int] = None
     crop_size_w: Optional[int] = None
-    resize_ratio0: Optional[float] = 1.0
-    resize_ratiof: Optional[float] = 1.5
+    resize_ratio0: Optional[float] = None
+    resize_ratiof: Optional[float] = None
     resize_add: Optional[float] = 1
     fliplr: Optional[float] = 0.5
     color_jitter: Optional[ColorJitter] = field(default_factory=lambda: ColorJitter())
     
-    def __post_init__(self):
-        # variable interpolation
-        if self.min_scale is None:
-            self.min_scale = self.img_size
-        if self.crop_size_h is None:
-            self.crop_size_h = self.img_size
-        if self.crop_size_w is None:
-            self.crop_size_w = self.img_size
+    
 
 @dataclass
 class ClassificationAugmentationConfig(AugmentationConfig):
@@ -46,6 +39,17 @@ class ClassificationAugmentationConfig(AugmentationConfig):
 @dataclass
 class SegmentationAugmentationConfig(AugmentationConfig):
     img_size = 512
+    resize_ratio0 = 1.0
+    resize_ratiof = 1.5
+    
+    def __post_init__(self):
+        # variable interpolation
+        if self.min_scale is None:
+            self.min_scale = self.img_size
+        if self.crop_size_h is None:
+            self.crop_size_h = self.img_size
+        if self.crop_size_w is None:
+            self.crop_size_w = self.img_size
     
 
 @dataclass
@@ -53,8 +57,13 @@ class DetectionAugmentationConfig(AugmentationConfig):
     img_size = 512
     max_scale = 2048
     min_scale = 768
-    crop_size_h = 512
-    crop_size_w = 512
     resize_ratio0: 0.5
     resize_ratiof: 2.0
     resize_add: 1
+    
+    def __post_init__(self):
+        # variable interpolation
+        if self.crop_size_h is None:
+            self.crop_size_h = self.img_size
+        if self.crop_size_w is None:
+            self.crop_size_w = self.img_size
