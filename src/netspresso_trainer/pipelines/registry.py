8b7833eebd4477ff00a1cba1afd25dc2a214ba36
Junho Shin <junho.shin@nota.ai>
2023-10-25 19:42:41 +0900

    Add TODO comment

--- src/netspresso_trainer/pipelines/registry.py
+++ src/netspresso_trainer/pipelines/registry.py
@@ -5,6 +5,7 @@ from .classification import ClassificationPipeline
 from .detection import TwoStageDetectionPipeline
 from .segmentation import SegmentationPipeline
 
+# TODO: Temporary defined. It should be integrated with `..models.registry.SUPPORTING_TASK_LIST`
 SUPPORTING_TASK_LIST: List[str] = ['classification', 'segmentation', 'detection']
 
 TASK_PIPELINE: Dict[str, Type[BasePipeline]]= {
