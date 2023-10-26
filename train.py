b790197034010588bdae5dc8a4fb216e0fc9c15b
deepkyu <hyoungkyu.song@nota.ai>
2023-10-26 15:25:00 +0900

    #203 update training configuration

--- train.py
+++ train.py
@@ -3,21 +3,40 @@ from netspresso_trainer import train_with_config, TrainerConfig
 
 if __name__ == '__main__':
     from netspresso_trainer.cfg import (
-        ClassificationAugmentationConfig,
         ClassificationResNetModelConfig,
-        ExampleBeansDataset,
-        ColorJitter
+        ExampleBeansDataset
     )
     
-    augmentation_config = ClassificationAugmentationConfig(color_jitter=ColorJitter(colorjitter_p=0.9))
+    """
+    Declare dataset config (Use an example dataset provided from the module)
+    """
     example_dataset = ExampleBeansDataset
+    
+    """
+    Declare model config
+    """
     example_model = ClassificationResNetModelConfig()
+    
+    # ### If you try to train torch.fx model from PyNetsPresso, use this block instead
+    # example_model = ClassificationResNetModelConfig(checkpoint=None)
+    # example_model.fx_model_checkpoint = "classification_resnet50_best_fx.pt"
+    # ###
+    
+    
+    """
+    Declare trainer config
+    """
     cfg = TrainerConfig(
         task='classification',
         auto=True,
         data=ExampleBeansDataset,
-        model=example_model,
-        augmentation=augmentation_config
+        model=example_model
     )
+    
+    """
+    Update major field values considering the spec of training machine
+    """
+    cfg.batch_size = 64
+    cfg.epochs = 5
     
     train_with_config(cfg, log_level='INFO')
