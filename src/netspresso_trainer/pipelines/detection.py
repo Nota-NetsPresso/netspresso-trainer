e56181f449b154927b44eea388ff3208aca90c51
Junho Shin <junho.shin@nota.ai>
2023-10-27 13:09:40 +0900

    Add postprocessing

--- src/netspresso_trainer/pipelines/detection.py
+++ src/netspresso_trainer/pipelines/detection.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import numpy as np
 import torch
+import torchvision
 from omegaconf import OmegaConf
 
 from ..models import build_model
@@ -229,9 +230,28 @@ class OneStageDetectionPipeline(BasePipeline):
         images = images.to(self.devices)
         targets = [{"boxes": box.to(self.devices), "labels": label.to(self.devices)}
                    for box, label in zip(bboxes, labels)]
+        
+        targets = {'gt': targets, 
+                   'img_size': images.size(-1), 
+                   'intermediate_features_dim': self.model.backbone.intermediate_features_dim,
+                   'num_classes': self.num_classes,}
+
+        self.optimizer.zero_grad()
 
         out = self.model(images)
-        self.loss_factory.calc(out, target=targets, phase='valid')
+        self.loss_factory.calc(out, targets, phase='valid')
+
+        # postprocess
+        self.hw = [x.shape[-2:] for x in out]
+        self.strides = [8, 16, 32]
+        # [batch, n_anchors_all, 85]
+        out = torch.cat(
+            [x.flatten(start_dim=2) for x in out], dim=2
+        ).permute(0, 2, 1)
+        out[..., 4:] = out[..., 4:].sigmoid()
+
+        #pred = self.decode_outputs(out, dtype=out[0].type())
+        pred = self.postprocess(out, self.num_classes)
 
         if self.conf.distributed:
             torch.distributed.barrier()
@@ -240,9 +260,13 @@ class OneStageDetectionPipeline(BasePipeline):
             'images': images.detach().cpu().numpy(),
             'target': [(bbox.detach().cpu().numpy(), label.detach().cpu().numpy())
                        for bbox, label in zip(bboxes, labels)],
-            'pred': [(np.concatenate((bbox.detach().cpu().numpy(), confidence.detach().cpu().numpy()[..., np.newaxis]), axis=-1),
-                      label.detach().cpu().numpy())
-                     for bbox, confidence, label in zip(out['post_boxes'], out['post_scores'], out['post_labels'])],
+            #'pred': [(np.concatenate((bbox.detach().cpu().numpy(), confidence.detach().cpu().numpy()[..., np.newaxis]), axis=-1),
+            #          label.detach().cpu().numpy())
+            #         for bbox, confidence, label in zip(out['post_boxes'], out['post_scores'], out['post_labels'])],
+            'pred': [(torch.cat([p[:, :4], p[:, 5:6]], dim=-1).detach().cpu().numpy(),
+                      p[:, 6].to(torch.int).detach().cpu().numpy()) 
+                      if p is not None else (np.array([[]]), np.array([]))
+                      for p in pred]
         }
         return dict(logs.items())
 
@@ -269,9 +293,79 @@ class OneStageDetectionPipeline(BasePipeline):
                 targets.append(target_on_image)
 
             for detection, class_idx in output_batch['pred']:
+                if detection.size == 0:
+                    continue
                 pred_on_image = {}
                 pred_on_image['post_boxes'] = detection[..., :4]
                 pred_on_image['post_scores'] = detection[..., -1]
                 pred_on_image['post_labels'] = class_idx
                 pred.append(pred_on_image)
         self.metric_factory.calc(pred, target=targets, phase='valid')
+
+    # TODO: Temporary defined in pipeline, it will be moved to postprocessor module.
+    def decode_outputs(self, outputs, dtype):
+        grids = []
+        strides = []
+        for (hsize, wsize), stride in zip(self.hw, self.strides):
+            yv, xv = torch.meshgrid(torch.arange(hsize), torch.arange(wsize), indexing='ij')
+            grid = torch.stack((xv, yv), 2).view(1, -1, 2)
+            grids.append(grid)
+            shape = grid.shape[:2]
+            strides.append(torch.full((*shape, 1), stride))
+
+        grids = torch.cat(grids, dim=1).type(dtype)
+        strides = torch.cat(strides, dim=1).type(dtype)
+
+        outputs = torch.cat([
+            (outputs[..., 0:2] + grids) * strides,
+            torch.exp(outputs[..., 2:4]) * strides,
+            outputs[..., 4:]
+        ], dim=-1)
+        return outputs
+
+    # TODO: Temporary defined in pipeline, it will be moved to postprocessor module.
+    def postprocess(self, prediction, num_classes, conf_thre=0.7, nms_thre=0.45, class_agnostic=False):
+        box_corner = prediction.new(prediction.shape)
+        box_corner[:, :, 0] = prediction[:, :, 0] - prediction[:, :, 2] / 2
+        box_corner[:, :, 1] = prediction[:, :, 1] - prediction[:, :, 3] / 2
+        box_corner[:, :, 2] = prediction[:, :, 0] + prediction[:, :, 2] / 2
+        box_corner[:, :, 3] = prediction[:, :, 1] + prediction[:, :, 3] / 2
+        prediction[:, :, :4] = box_corner[:, :, :4]
+
+        output = [None for _ in range(len(prediction))]
+        for i, image_pred in enumerate(prediction):
+
+            # If none are remaining => process next image
+            if not image_pred.size(0):
+                continue
+            # Get score and class with highest confidence
+            class_conf, class_pred = torch.max(image_pred[:, 5: 5 + num_classes], 1, keepdim=True)
+
+            conf_mask = (image_pred[:, 4] * class_conf.squeeze() >= conf_thre).squeeze()
+            # Detections ordered as (x1, y1, x2, y2, obj_conf, class_conf, class_pred)
+            detections = torch.cat((image_pred[:, :5], class_conf, class_pred.float()), 1)
+            detections = detections[conf_mask]
+            if not detections.size(0):
+                continue
+
+            if class_agnostic:
+                nms_out_index = torchvision.ops.nms(
+                    detections[:, :4],
+                    detections[:, 4] * detections[:, 5],
+                    nms_thre,
+                )
+            else:
+                nms_out_index = torchvision.ops.batched_nms(
+                    detections[:, :4],
+                    detections[:, 4] * detections[:, 5],
+                    detections[:, 6],
+                    nms_thre,
+                )
+
+            detections = detections[nms_out_index]
+            if output[i] is None:
+                output[i] = detections
+            else:
+                output[i] = torch.cat((output[i], detections))
+
+        return output
