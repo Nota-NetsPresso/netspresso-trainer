df95b148f0e5e1b8d66aba94e3b40db5c8fb5e06
deepkyu <hyoungkyu.song@nota.ai>
2023-10-30 20:27:31 +0900

    #207 check multi-gpu training with entrypoint

--- src/netspresso_trainer/trainer.py
+++ src/netspresso_trainer/trainer.py
@@ -1,19 +1,66 @@
 import argparse
 import os
 from typing import Literal
+import subprocess
+import sys
+
 
 from omegaconf import DictConfig, OmegaConf
 
-from .cfg import TrainerConfig
-from .trainer_common import train_common
+from netspresso_trainer.cfg import TrainerConfig
+from netspresso_trainer.trainer_common import train_common
 
 LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
 
-def parse_args_netspresso():
+
+def run_distributed_training_script(gpu_ids, command):
+    
+    # Distributed training script
+    command = [
+        'python', '-m', 'torch.distributed.launch',
+        f'--nproc_per_node={len(gpu_ids)}',  # GPU #
+        os.path.abspath(__file__), *command
+    ]
+    print(command)
+
+    # subprocess 실행
+    process = subprocess.Popen(command)
+
+    try:
+        # Ctrl+C로 종료되기 전까지 대기
+        process.wait()
+    except KeyboardInterrupt:
+        # Ctrl+C로 인터럽트 발생 시 graceful shutdown을 위해 프로세스 종료
+        print("Interrupted. Terminating the training process...")
+        process.terminate()
+        process.wait()
+
+
+def parse_gpu_ids(gpu_arg):
+    """Parse comma-separated GPU IDs and return as a list of integers."""
+    try:
+        gpu_ids = [int(id) for id in gpu_arg.split(',')]
+        
+        if len(gpu_ids) == 1:  # Single GPU
+            return gpu_ids[0]
+        
+        gpu_ids = sorted(gpu_ids)
+        return gpu_ids
+    except ValueError:
+        raise argparse.ArgumentTypeError('Invalid GPU IDs. Please provide comma-separated integers.')
+
+
+def parse_args_netspresso(with_gpus=False):
 
     parser = argparse.ArgumentParser(description="Parser for NetsPresso configuration")
 
     # -------- User arguments ----------------------------------------
+    
+    if with_gpus:
+        parser.add_argument(
+            '--gpus', type=parse_gpu_ids, default=0,
+            dest='gpus',
+            help='GPU device indices (comma-separated)')
 
     parser.add_argument(
         '--data', type=str, required=True,
@@ -55,6 +102,27 @@ def parse_args_netspresso():
     return args_parsed
 
 
+def train_with_yaml() -> None:
+    args_parsed = parse_args_netspresso(with_gpus=True)
+    
+    assert isinstance(args_parsed.gpus, (list, int))
+    if isinstance(args_parsed.gpus, list):
+        gpu_ids_str = ','.join(map(str, args_parsed.gpus))
+    else:
+        gpu_ids_str = str(args_parsed.gpus)
+    os.environ['CUDA_VISIBLE_DEVICES'] = gpu_ids_str
+    
+    if isinstance(args_parsed.gpus, int):
+        conf = set_arguments(args_parsed)
+        train_common(conf, log_level=args_parsed.log_level)
+    else:
+        run_distributed_training_script(args_parsed.gpus, command=sys.argv[2:])
+        
+def train_with_yaml_without_additional_gpu_check() -> None:
+    args_parsed = parse_args_netspresso()
+    conf = set_arguments(args_parsed)
+    train_common(conf, log_level=args_parsed.log_level)
+
 def set_arguments(args_parsed) -> DictConfig:
     conf_data = OmegaConf.load(args_parsed.data)
     conf_augmentation = OmegaConf.load(args_parsed.augmentation)
@@ -83,11 +151,6 @@ def export_config_as_yaml(config: TrainerConfig) -> str:
     conf: DictConfig = OmegaConf.create(config)
     return OmegaConf.to_yaml(conf)
 
-def train_with_yaml() -> None:
-    args_parsed = parse_args_netspresso()
-    conf = set_arguments(args_parsed)
-    train_common(conf, log_level=args_parsed.log_level)
-
 def train_with_config(config: TrainerConfig, log_level: Literal['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'] = 'INFO') -> None:
     conf: DictConfig = OmegaConf.create(config)
     set_struct_recursive(conf, False)
@@ -95,4 +158,4 @@ def train_with_config(config: TrainerConfig, log_level: Literal['DEBUG', 'INFO',
 
 
 if __name__ == "__main__":
-    train_with_yaml()
+    train_with_yaml_without_additional_gpu_check()
