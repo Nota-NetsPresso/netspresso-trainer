0f49f7361b642d733e41bebe9ab197d2fea12107
deepkyu <hyoungkyu.song@nota.ai>
2023-10-31 19:29:59 +0900

    #207 fix example training script

--- scripts/example_train.sh
+++ scripts/example_train.sh
@@ -10,13 +10,13 @@ cd "$(dirname ${0})/.."
 
 #### Classification
 
-netspresso-train\
-  --data config/data/beans.yaml\
-  --augmentation config/augmentation/classification.yaml\
-  --model config/model/resnet/resnet50-classification.yaml\
-  --training config/training/classification.yaml\
-  --logging config/logging.yaml\
-  --environment config/environment.yaml
+# netspresso-train\
+#   --data config/data/beans.yaml\
+#   --augmentation config/augmentation/classification.yaml\
+#   --model config/model/resnet/resnet50-classification.yaml\
+#   --training config/training/classification.yaml\
+#   --logging config/logging.yaml\
+#   --environment config/environment.yaml
 
 # netspresso-train\
 #   --data config/data/beans.yaml\
@@ -107,34 +107,11 @@ netspresso-train\
 # Put the number of GPU(s) to use in training at `--nproc_per_node`
 #### (END)
 
-# python -m torch.distributed.launch\
-#   --nproc_per_node 4\
-#   train.py\
-#   --data config/data/chess.yaml\
+# netspresso-train\
+#   --gpus 0,1,2,3\
+#   --data config/data/beans.yaml\
 #   --augmentation config/augmentation/classification.yaml\
 #   --model config/model/resnet/resnet50-classification.yaml\
 #   --training config/training/classification.yaml\
-#   --logging config/logging.yaml\
-#   --environment config/environment.yaml
-
-
-# python -m torch.distributed.launch\
-#   --nproc_per_node 4\
-#   train.py\
-#   --data config/data/sidewalk-semantic.yaml\
-#   --augmentation config/augmentation/segmentation.yaml\
-#   --model config/model/pidnet/pidnet-s-segmentation.yaml\
-#   --training config/training/segmentation.yaml\
-#   --logging config/logging.yaml\
-#   --environment config/environment.yaml
-
-
-# python -m torch.distributed.launch\
-#   --nproc_per_node 4\
-#   train.py\
-#   --data config/data/traffic-sign.yaml\
-#   --augmentation config/augmentation/detection.yaml\
-#   --model config/model/efficientformer/efficientformer-l1-detection.yaml\
-#   --training config/training/detection.yaml\
 #   --logging config/logging.yaml\
 #   --environment config/environment.yaml
