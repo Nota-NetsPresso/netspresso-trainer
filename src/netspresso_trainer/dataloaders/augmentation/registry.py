36454dae6791309584c262617af02c84d5fdbba1
Junho Shin <junho.shin@nota.ai>
2023-11-01 15:40:57 +0900

    Update TRANSFORM_DICT

--- src/netspresso_trainer/dataloaders/augmentation/registry.py
+++ src/netspresso_trainer/dataloaders/augmentation/registry.py
@@ -1,8 +1,13 @@
 from typing import Callable, Dict
 
-from .custom import RandomHorizontalFlip, RandomResizedCrop
+from .custom import ColorJitter, Pad, RandomCrop, RandomHorizontalFlip, RandomResizedCrop, RandomVerticalFlip, Resize
 
 TRANSFORM_DICT: Dict[str, Callable] = {
+    'colorjitter': ColorJitter,
+    'pad': Pad,
+    'randomcrop': RandomCrop,
     'randomresizedcrop': RandomResizedCrop,
-    'randomhorizontalflip': RandomHorizontalFlip
+    'randomhorizontalflip': RandomHorizontalFlip,
+    'randomVerticalFlip': RandomVerticalFlip,
+    'resize': Resize,
 }
