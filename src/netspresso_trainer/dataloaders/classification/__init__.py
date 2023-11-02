9537d55795ee24a1bd35db4ab09bf34311d6e7c1
Junho Shin <junho.shin@nota.ai>
2023-11-02 11:39:37 +0900

    Remove error

--- src/netspresso_trainer/dataloaders/classification/__init__.py
+++ src/netspresso_trainer/dataloaders/classification/__init__.py
@@ -1,4 +1,3 @@
 from .dataset import ClassficationDataSampler
 from .huggingface import ClassificationHFDataset
 from .local import ClassificationCustomDataset
-from .transforms import create_transform_classification
