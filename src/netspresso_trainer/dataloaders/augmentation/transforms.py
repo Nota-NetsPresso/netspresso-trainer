f3a11a66cbd212ffb9e0b732a1790c3517722a39
Junho Shin <junho.shin@nota.ai>
2023-11-02 11:06:20 +0900

    Leave pidnet as exception

--- src/netspresso_trainer/dataloaders/augmentation/transforms.py
+++ src/netspresso_trainer/dataloaders/augmentation/transforms.py
@@ -33,5 +33,34 @@ def transforms_custom_eval(conf_augmentation):
     return TC.Compose(preprocess)
 
 
+def train_transforms_pidnet(conf_augmentation):
+    preprocess = []
+    for augment in conf_augmentation.recipe:
+        name = augment.name.lower()
+        augment_kwargs = list(augment.keys())
+        augment_kwargs.remove('name')
+        augment_kwargs = {k:augment[k] for k in augment_kwargs}
+        transform = TRANSFORM_DICT[name](**augment_kwargs)
+        preprocess.append(transform)
+
+    preprocess = preprocess + [
+        TC.ToTensor(),
+        TC.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD)
+    ]
+    return TC.Compose(preprocess, additional_targets={'edge': 'mask'})
+
+
+def val_transforms_pidnet(conf_augmentation):
+    assert conf_augmentation.img_size > 32
+    preprocess = [
+        TC.Resize((conf_augmentation.img_size, conf_augmentation.img_size), interpolation='bilinear'),
+        TC.ToTensor(),
+        TC.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD)
+    ]
+    return TC.Compose(preprocess, additional_targets={'edge': 'mask'})
+
+
 def create_transform(model_name: str, is_training=False):
+    if model_name == 'pidnet':
+        return train_transforms_pidnet if is_training else val_transforms_pidnet
     return transforms_custom_train if is_training else transforms_custom_eval
