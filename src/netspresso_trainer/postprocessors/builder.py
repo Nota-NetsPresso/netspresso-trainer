2f22333dc338e7d76afd40c3d1a0e22b0b178b21
Junho Shin <junho.shin@nota.ai>
2023-11-07 13:27:48 +0900

    Temporary return None for 2-stage detector

--- src/netspresso_trainer/postprocessors/builder.py
+++ src/netspresso_trainer/postprocessors/builder.py
@@ -3,4 +3,6 @@ from .register import POSTPROCESSOR_DICT
 
 def build_postprocessor(task: str, conf_model):
     head_name = conf_model.architecture.head.name
+    if head_name not in POSTPROCESSOR_DICT:
+        return None
     return POSTPROCESSOR_DICT[head_name]()
