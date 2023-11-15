6ee84f77220871e89fd4587bbdfe6d07f464892e
Junho Shin <junho.shin@nota.ai>
2023-11-15 19:00:19 +0900

    Handle postprocessor for full model

--- src/netspresso_trainer/postprocessors/builder.py
+++ src/netspresso_trainer/postprocessors/builder.py
@@ -2,7 +2,10 @@ from .register import POSTPROCESSOR_DICT
 
 
 def build_postprocessor(task: str, conf_model):
-    head_name = conf_model.architecture.head.name
+    if conf_model.single_task_model:
+        head_name = conf_model.architecture.full.name
+    else:
+        head_name = conf_model.architecture.head.name
     if head_name not in POSTPROCESSOR_DICT:
         return None
     return POSTPROCESSOR_DICT[head_name](conf_model)
