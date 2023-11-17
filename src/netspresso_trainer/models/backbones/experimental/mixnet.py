ca9ec6eb437b753692e6c266b76edf19fd862de6
Junho Shin <junho.shin@nota.ai>
2023-11-17 12:59:52 +0900

    Fix MixNet to stage format

--- src/netspresso_trainer/models/backbones/experimental/mixnet.py
+++ src/netspresso_trainer/models/backbones/experimental/mixnet.py
@@ -167,39 +167,51 @@ class MixNet(nn.Module):
 
         in_channels = out_channels
         drop_rate = self.dropout_rate
-        mod_id = 0
-        for t, c, n, k, ek, pk, s, d, a, se in settings:
-            out_channels = self._round_filters(c, width_multi)
-            repeats = self._round_repeats(n, depth_multi)
+        stages: List[nn.Module] = []
+        for stg_idx, blocks in enumerate(stage_params):
+            
+            stage: List[nn.Module] = []
+            for block in blocks:
+                t, c, n, k, ek, pk, s, d, a, se = block
+                out_channels = self._round_filters(c, width_multi)
+                repeats = self._round_repeats(n, depth_multi)
+                # Create blocks for module
+                blocks = []
+                for block_id in range(repeats):
+                    stride = s if block_id == 0 else 1
+                    dilate = d if stride == 1 else 1
 
-            if self.dropout_rate:
-                drop_rate = self.dropout_rate * float(mod_id + 1) / len(settings)
+                    stage.append(MixDepthBlock(in_channels, out_channels,
+                                               expand_ratio=t, exp_kernel_sizes=ek,
+                                               kernel_sizes=k, poi_kernel_sizes=pk,
+                                               stride=stride, dilate=dilate,
+                                               reduction_ratio=se,
+                                               dropout_rate=drop_rate,
+                                               act_type=a))
 
-            # Create blocks for module
-            blocks = []
-            for block_id in range(repeats):
-                stride = s if block_id == 0 else 1
-                dilate = d if stride == 1 else 1
+                    in_channels = out_channels
+            
+            # add last conv
+            if stg_idx == len(stage_params) - 1:
+                self.last_channels = 1536
+                stage.append(
+                    ConvLayer(in_channels=in_channels,
+                              out_channels=self.last_channels,
+                              kernel_size=1,
+                              stride=1,
+                              groups=1,
+                              dilation=1,
+                              act_type="relu")
+                )
+            
+            stage = nn.Sequential(*stage)
+            stages.append(stage)
 
-                blocks.append(("block%d" % (block_id + 1), MixDepthBlock(in_channels, out_channels,
-                                                                         expand_ratio=t, exp_kernel_sizes=ek,
-                                                                         kernel_sizes=k, poi_kernel_sizes=pk,
-                                                                         stride=stride, dilate=dilate,
-                                                                         reduction_ratio=se,
-                                                                         dropout_rate=drop_rate,
-                                                                         act_type=a)))
-
-                in_channels = out_channels
-            self.add_module("mod%d" % (mod_id + 2), nn.Sequential(OrderedDict(blocks)))
-            mod_id += 1
-
-        self.last_channels = 1536
-        self.last_feat = ConvLayer(in_channels=in_channels, out_channels=self.last_channels,
-                                   kernel_size=1, stride=1, groups=1, dilation=1, act_type="relu")
-        
+        self.stages = nn.ModuleList(stages)
         self.avgpool = nn.AdaptiveAvgPool2d(1)
 
         self._feature_dim = self.last_channels
+        self.use_intermediate_features = False
 
         self._initialize_weights()
 
@@ -247,12 +259,16 @@ class MixNet(nn.Module):
         return self._intermediate_features_dim
 
     def forward(self, x):
-        x = self.mod2(self.mod1(x))  # (N, C,   H/2,  W/2)
-        x = self.mod4(self.mod3(x))  # (N, C,   H/4,  W/4)
-        x = self.mod6(self.mod5(x))  # (N, C,   H/8,  W/8)
-        x = self.mod10(self.mod9(self.mod8(self.mod7(x))))  # (N, C,   H/16, W/16)
-        x = self.mod12(self.mod11(x))  # (N, C,   H/32, W/32)
-        x = self.last_feat(x)
+        x = self.mod1(x)
+
+        all_hidden_states = () if self.use_intermediate_features else None
+        for stage in self.stages:
+            x = stage(x)
+            if self.use_intermediate_features:
+                all_hidden_states = all_hidden_states + (x, )
+        
+        if self.use_intermediate_features:
+            return BackboneOutput(intermediate_features=all_hidden_states)
 
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
