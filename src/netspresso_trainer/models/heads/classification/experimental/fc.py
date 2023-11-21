e7f18a9c05868b11f4d37d7e9ed9d13bbcd12c50
Junho Shin <junho.shin@nota.ai>
2023-11-21 14:35:39 +0900

    Change FC head to receive DictConfig

--- src/netspresso_trainer/models/heads/classification/experimental/fc.py
+++ src/netspresso_trainer/models/heads/classification/experimental/fc.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+from omegaconf import DictConfig
 import torch
 import torch.nn as nn
 from torch import Tensor
@@ -9,13 +10,22 @@ from ....utils import ModelOutput
 
 
 class FC(nn.Module):
-    def __init__(self, feature_dim: int, num_classes: int) -> None:
+    def __init__(self, feature_dim: int, num_classes: int, params: DictConfig) -> None:
         super(FC, self).__init__()
-        self.classifier = nn.Linear(feature_dim, num_classes)
+        hidden_size = params.hidden_size
+        hidden_layers = params.hidden_layers
+
+        prev_size = feature_dim
+        classifier = []
+        for _ in range(hidden_layers):
+            classifier.append(nn.Linear(prev_size, hidden_size))
+            prev_size = hidden_size
+        classifier.append(nn.Linear(prev_size, num_classes))
+        self.classifier = nn.Sequential(*classifier)
         
     def forward(self, x: Union[Tensor, Proxy]) -> ModelOutput:
         x = self.classifier(x)
         return ModelOutput(pred=x)
     
-def fc(feature_dim, num_classes, **kwargs):
-    return FC(feature_dim=feature_dim, num_classes=num_classes)
+def fc(feature_dim, num_classes, conf_model_head, **kwargs) -> FC:
+    return FC(feature_dim=feature_dim, num_classes=num_classes, params=conf_model_head.params)
