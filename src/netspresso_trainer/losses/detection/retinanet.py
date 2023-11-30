9a8fa0d2693e5ce118528db622d9471a33417561
Junho Shin <junho.shin@nota.ai>
2023-11-30 19:03:12 +0900

    Implement RetinaNetClassificationLoss

--- src/netspresso_trainer/losses/detection/retinanet.py
+++ src/netspresso_trainer/losses/detection/retinanet.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from torch import Tensor
 from torchvision.ops import boxes as box_ops
 
+from ..common import SigmoidFocalLoss
 from ...models.heads.detection.experimental.detection._utils import Matcher
 
 
@@ -19,12 +20,12 @@ class RetinaNetLoss(nn.Module):
                 bg_iou_thresh,
                 allow_low_quality_matches=True,
         )
-        self.cls_loss = RetinaNetClassificationLoss()
+        self.cls_loss = RetinaNetClassificationLoss(self.proposal_matcher.BETWEEN_THRESHOLDS)
         self.reg_loss = RetinaNetRegressionLoss()
     
     def compute_loss(self, out, target, matched_idxs):
-        cls_loss = self.classification_head.compute_loss(out, target, matched_idxs)
-        box_loss = self.regression_head.compute_loss(out, target, matched_idxs)
+        cls_loss = self.cls_loss(out, target, matched_idxs)
+        box_loss = self.reg_loss(out, target, matched_idxs)
 
         # TODO: return as dict
         return cls_loss + box_loss
@@ -47,11 +48,45 @@ class RetinaNetLoss(nn.Module):
 
 
 class RetinaNetClassificationLoss(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, between_threshold) -> None:
         super().__init__()
+        self.between_threshold = between_threshold
+        # TODO: Get from config
+        alpha = 0.25
+        gamma = 2
+        self.focal_loss = SigmoidFocalLoss(alpha=alpha, gamma=gamma)
     
-    def forward(self, target, out, matched_idxs):
-        pass
+    def forward(self, out, target, matched_idxs):
+        losses = []
+
+        cls_logits = out["cls_logits"]
+
+        for targets_per_image, cls_logits_per_image, matched_idxs_per_image in zip(target['gt'], cls_logits, matched_idxs):
+            # determine only the foreground
+            foreground_idxs_per_image = matched_idxs_per_image >= 0
+            num_foreground = foreground_idxs_per_image.sum()
+
+            # create the target classification
+            gt_classes_target = torch.zeros_like(cls_logits_per_image)
+            gt_classes_target[
+                foreground_idxs_per_image,
+                targets_per_image["labels"][matched_idxs_per_image[foreground_idxs_per_image]],
+            ] = 1.0
+
+            # find indices for which anchors should be ignored
+            valid_idxs_per_image = matched_idxs_per_image != self.between_threshold
+
+            # compute the classification loss
+            losses.append(
+                self.focal_loss(
+                    {'pred': cls_logits_per_image[valid_idxs_per_image]},
+                    gt_classes_target[valid_idxs_per_image],
+                    reduction="sum",
+                )
+                / max(1, num_foreground)
+            )
+
+        return sum(losses) / len(target['gt'])
 
 
 class RetinaNetRegressionLoss(nn.Module):
