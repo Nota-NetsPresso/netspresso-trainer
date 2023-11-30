195c6ee361d21efc82719f1f432868b38a7083e7
Junho Shin <junho.shin@nota.ai>
2023-11-30 17:47:29 +0900

    Implement gt-prediction matching

--- src/netspresso_trainer/losses/detection/retinanet.py
+++ src/netspresso_trainer/losses/detection/retinanet.py
@@ -4,12 +4,35 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
+from torchvision.ops import boxes as box_ops
+
+from ...models.heads.detection.experimental.detection._utils import Matcher
 
 
 class RetinaNetLoss(nn.Module):
     def __init__(self) -> None:
         super().__init__()
+        fg_iou_thresh = 0.5
+        bg_iou_thresh = 0.4
+        self.proposal_matcher = Matcher(
+                fg_iou_thresh,
+                bg_iou_thresh,
+                allow_low_quality_matches=True,
+        )
+        self.compute_loss = None
 
-    @staticmethod
-    def forward(out: Dict, target: torch.Tensor) -> torch.Tensor:
-        return None
+    def forward(self, out: Dict, target: torch.Tensor) -> torch.Tensor:
+        matched_idxs = []
+        anchors = out['anchors']
+
+        for targets_per_image in target['gt']:
+            if targets_per_image["boxes"].numel() == 0:
+                matched_idxs.append(
+                    torch.full((anchors.size(0),), -1, dtype=torch.int64, device=anchors.device)
+                )
+                continue
+
+            match_quality_matrix = box_ops.box_iou(targets_per_image["boxes"], anchors)
+            matched_idxs.append(self.proposal_matcher(match_quality_matrix))
+
+        return self.compute_loss(target, out, matched_idxs)
