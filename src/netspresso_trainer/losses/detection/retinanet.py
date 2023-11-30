4ec775b59599281320c293e413caa5db5cfa5d9e
Junho Shin <junho.shin@nota.ai>
2023-11-30 17:54:32 +0900

    Init RetinaNet cls_loss and box_loss

--- src/netspresso_trainer/losses/detection/retinanet.py
+++ src/netspresso_trainer/losses/detection/retinanet.py
@@ -19,7 +19,15 @@ class RetinaNetLoss(nn.Module):
                 bg_iou_thresh,
                 allow_low_quality_matches=True,
         )
-        self.compute_loss = None
+        self.cls_loss = RetinaNetClassificationLoss()
+        self.reg_loss = RetinaNetRegressionLoss()
+    
+    def compute_loss(self, out, target, matched_idxs):
+        cls_loss = self.classification_head.compute_loss(out, target, matched_idxs)
+        box_loss = self.regression_head.compute_loss(out, target, matched_idxs)
+
+        # TODO: return as dict
+        return cls_loss + box_loss
 
     def forward(self, out: Dict, target: torch.Tensor) -> torch.Tensor:
         matched_idxs = []
@@ -35,4 +43,20 @@ class RetinaNetLoss(nn.Module):
             match_quality_matrix = box_ops.box_iou(targets_per_image["boxes"], anchors)
             matched_idxs.append(self.proposal_matcher(match_quality_matrix))
 
-        return self.compute_loss(target, out, matched_idxs)
+        return self.compute_loss(out, target, matched_idxs)
+
+
+class RetinaNetClassificationLoss(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self, target, out, matched_idxs):
+        pass
+
+
+class RetinaNetRegressionLoss(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def forward(self, target, out, matched_idxs):
+        pass
