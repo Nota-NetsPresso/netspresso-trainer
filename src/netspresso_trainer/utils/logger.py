fb5322cc8a47a1b2adcf9516ea1caaf0cb7e4137
deepkyu <hyoungkyu.song@nota.ai>
2023-12-06 20:50:08 +0900

    #250 replace with loguru logger

--- src/netspresso_trainer/utils/logger.py
+++ src/netspresso_trainer/utils/logger.py
@@ -1,63 +1,46 @@
-import logging
+# import logging
+import sys
 import time
 from typing import Literal
 
 import torch.distributed as dist
 from omegaconf import DictConfig, ListConfig, OmegaConf
+from loguru import logger
 
 __all__ = ['set_logger', 'yaml_for_logging']
 ROOT_LOGGER_NAME = "netspresso_trainer"
 
-class RankFilter(logging.Filter):
-    def filter(self, record):
-        try:
-            return dist.get_rank() == 0
-        except RuntimeError:  # Default process group has not been initialized, please make sure to call init_process_group.
-            return True
+def rank_filter(record):
+    try:
+        return dist.get_rank() == 0
+    except RuntimeError:  # Default process group has not been initialized, please make sure to call init_process_group.
+        return True
 
-def get_format(name: str, level: str, distributed: bool):
-    fmt_date = '%Y-%m-%d_%T %Z'
+def get_format(level: str = "INFO", distributed: bool = False):
     debug_and_multi_gpu = (level == 'DEBUG' and distributed)
+    fmt = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+    
     if debug_and_multi_gpu:
-        fmt = f'[GPU:{dist.get_rank()}] %(asctime)s | %(levelname)s\t\t| %(funcName)s:<%(filename)s>:%(lineno)s >>> %(message)s'
-    else:
-        fmt = '%(asctime)s | %(levelname)s\t\t| %(funcName)s:<%(filename)s>:%(lineno)s >>> %(message)s'
+        fmt = f"[GPU:{dist.get_rank()}]" + fmt
+    
+    return fmt
 
-    return fmt, fmt_date
+def add_stream_handler(level: str, distributed: bool):
+    fmt = get_format(level, distributed=distributed)
+    logger.add(sys.stderr, level=level, format=fmt, filter=rank_filter)
 
-def add_stream_handler(distributed: bool):
-    logger = logging.getLogger(ROOT_LOGGER_NAME)
-    handler = logging.StreamHandler()
-    fmt, fmt_date = get_format(logger.name, logger.level, distributed=distributed)
-
-    formatter = logging.Formatter(fmt, fmt_date)
-    handler.setFormatter(formatter)
-    if distributed:
-        handler.addFilter(RankFilter())
-
-    logger.addHandler(handler)
 
 
 def add_file_handler(log_filepath: str, distributed: bool):
-    logger = logging.getLogger(ROOT_LOGGER_NAME)
-    handler = logging.FileHandler(log_filepath)
-
-    fmt, fmt_date = get_format(logger.name, logger.level, distributed=distributed)
-    formatter = logging.Formatter(fmt, fmt_date)
-    handler.setFormatter(formatter)
-    if distributed:
-        handler.addFilter(RankFilter())
-
-    logger.addHandler(handler)
+    level = logger._core.min_level
+    fmt = get_format(level, distributed=distributed)
+    logger.add(log_filepath, format=fmt, filter=rank_filter, level=level)
 
 
 
-def _custom_logger(name: str, level: str, distributed: bool):
-    logger = logging.getLogger(name)
-
-    logger.setLevel(logging.INFO)
-    if not logger.hasHandlers():
-        add_stream_handler(distributed)
+def _custom_logger(level: str, distributed: bool):
+    logger.remove()
+    add_stream_handler(level, distributed)
 
     return logger
 
@@ -70,19 +53,8 @@ def set_logger(level: str = 'INFO', distributed=False):
         print(e)
         print("Skipping timezone setting.")
     _level: Literal['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'] = level.upper()
-    _custom_logger(ROOT_LOGGER_NAME, _level, distributed)
+    _custom_logger(_level, distributed)
 
-    logger = logging.getLogger(ROOT_LOGGER_NAME)
-    if _level == 'DEBUG':
-        logger.setLevel(logging.DEBUG)
-    elif _level == 'INFO':
-        logger.setLevel(logging.INFO)
-    elif _level == 'WARNING':
-        logger.setLevel(logging.WARNING)
-    elif _level == 'ERROR':
-        logger.setLevel(logging.ERROR)
-    elif _level == 'CRITICAL':
-        logger.setLevel(logging.CRITICAL)
     return logger
 
 
