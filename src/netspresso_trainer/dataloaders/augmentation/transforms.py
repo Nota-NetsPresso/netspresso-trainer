824373e29b283e0cc0f55f16bccf1c1c433375f1
Junho Shin <junho.shin@nota.ai>
2023-12-26 17:43:03 +0900

    Fix eval resize parameter bug

--- src/netspresso_trainer/dataloaders/augmentation/transforms.py
+++ src/netspresso_trainer/dataloaders/augmentation/transforms.py
@@ -49,7 +49,7 @@ def transforms_custom_train(conf_augmentation):
 def transforms_custom_eval(conf_augmentation):
     assert conf_augmentation.img_size > 32
     preprocess = [
-        TC.Resize((conf_augmentation.img_size, conf_augmentation.img_size), interpolation='bilinear'),
+        TC.Resize((conf_augmentation.img_size, conf_augmentation.img_size), interpolation='bilinear', max_size=None),
         TC.ToTensor(),
         TC.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD)
     ]
@@ -76,7 +76,7 @@ def train_transforms_pidnet(conf_augmentation):
 def val_transforms_pidnet(conf_augmentation):
     assert conf_augmentation.img_size > 32
     preprocess = [
-        TC.Resize((conf_augmentation.img_size, conf_augmentation.img_size), interpolation='bilinear'),
+        TC.Resize((conf_augmentation.img_size, conf_augmentation.img_size), interpolation='bilinear', max_size=None),
         TC.ToTensor(),
         TC.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD)
     ]
