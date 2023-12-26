465a4770aa7c73f64a487e3e38c21a509d77de94
Junho Shin <junho.shin@nota.ai>
2023-12-26 16:44:41 +0900

    Modify scheduler builder

--- src/netspresso_trainer/schedulers/builder.py
+++ src/netspresso_trainer/schedulers/builder.py
@@ -5,18 +5,14 @@ from .registry import SCHEDULER_DICT
 
 
 def build_scheduler(optimizer, conf_training):
-    scheduler_name = conf_training.sched
+    conf_scheduler = conf_training.scheduler
+    scheduler_name = conf_scheduler.name
     num_epochs = conf_training.epochs
-    conf_sched = OmegaConf.create({
-        'min_lr': conf_training.min_lr,
-        'power': conf_training.sched_power,
-        'warmup_bias_lr': conf_training.warmup_bias_lr,
-        'warmup_iters': conf_training.warmup_epochs,
-        'total_iters': num_epochs,
-        'iters_per_phase': conf_training.iters_per_phase,  # TODO: config for StepLR
-    })
+
+    # Copy training num_epochs to sub-config scheduler
+    conf_scheduler.total_iters = num_epochs
 
     assert scheduler_name in SCHEDULER_DICT, f"{scheduler_name} not in scheduler dict!"
-    lr_scheduler = SCHEDULER_DICT[scheduler_name](optimizer, **conf_sched)
+    lr_scheduler = SCHEDULER_DICT[scheduler_name](optimizer, conf_scheduler)
 
     return lr_scheduler, num_epochs
