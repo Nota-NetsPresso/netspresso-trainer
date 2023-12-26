c6aec25b25f617b055b9df27498573cc6b189348
Junho Shin <junho.shin@nota.ai>
2023-12-27 01:29:09 +0900

    Rename scheduler builder conf variables

--- src/netspresso_trainer/schedulers/builder.py
+++ src/netspresso_trainer/schedulers/builder.py
@@ -4,15 +4,15 @@ from omegaconf import OmegaConf
 from .registry import SCHEDULER_DICT
 
 
-def build_scheduler(optimizer, conf_training):
-    conf_scheduler = conf_training.scheduler
-    scheduler_name = conf_scheduler.name
-    num_epochs = conf_training.epochs
+def build_scheduler(optimizer, training_conf):
+    scheduler_conf = training_conf.scheduler
+    scheduler_name = scheduler_conf.name
+    num_epochs = training_conf.epochs
 
     # Copy training num_epochs to sub-config scheduler
-    conf_scheduler.total_iters = num_epochs # fix total_iters as num_epochs
+    scheduler_conf.total_iters = num_epochs # fix total_iters as num_epochs
 
     assert scheduler_name in SCHEDULER_DICT, f"{scheduler_name} not in scheduler dict!"
-    lr_scheduler = SCHEDULER_DICT[scheduler_name](optimizer, conf_scheduler)
+    lr_scheduler = SCHEDULER_DICT[scheduler_name](optimizer, scheduler_conf)
 
     return lr_scheduler, num_epochs
