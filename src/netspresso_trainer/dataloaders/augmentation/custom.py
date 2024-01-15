2f3d775e49acebf287a11cbe44a8238058d1431d
Junho Shin <junho.shin@nota.ai>
2024-01-15 13:54:44 +0900

    Add CenterCrop transform

--- src/netspresso_trainer/dataloaders/augmentation/custom.py
+++ src/netspresso_trainer/dataloaders/augmentation/custom.py
@@ -64,6 +64,22 @@ class Compose:
         return compose_summary
 
 
+class CenterCrop(T.CenterCrop):
+
+    def __init__(
+        self,
+        size: Union[int, List],
+    ):
+        super().__init__(size)
+
+    def forward(self, image, mask=None, bbox=None):
+        # TODO: Compute mask, bbox
+        return F.center_crop(image, self.size), mask, bbox
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(size={self.size})"
+
+
 class Identity:
     visualize = True
 
