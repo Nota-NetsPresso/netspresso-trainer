453447b6d84036ea10910be363911eb78a06e5de
Junho Shin <junho.shin@nota.ai>
2024-01-15 10:27:50 +0900

    Fix tranform function for PIDNet

--- src/netspresso_trainer/dataloaders/augmentation/transforms.py
+++ src/netspresso_trainer/dataloaders/augmentation/transforms.py
@@ -51,15 +51,18 @@ def transforms_custom(conf_augmentation, training):
     return TC.Compose(preprocess)
 
 
-def train_transforms_pidnet(conf_augmentation):
+def train_transforms_pidnet(conf_augmentation, training):
     preprocess = []
-    for augment in conf_augmentation.transforms:
-        name = augment.name.lower()
-        augment_kwargs = list(augment.keys())
-        augment_kwargs.remove('name')
-        augment_kwargs = {k:augment[k] for k in augment_kwargs}
-        transform = TRANSFORM_DICT[name](**augment_kwargs)
-        preprocess.append(transform)
+    phase_conf = conf_augmentation.train if training else conf_augmentation.inference
+
+    if phase_conf.transforms:
+        for augment in phase_conf.transforms:
+            name = augment.name.lower()
+            augment_kwargs = list(augment.keys())
+            augment_kwargs.remove('name')
+            augment_kwargs = {k:augment[k] for k in augment_kwargs}
+            transform = TRANSFORM_DICT[name](**augment_kwargs)
+            preprocess.append(transform)
 
     preprocess = preprocess + [
         TC.ToTensor(),
@@ -68,17 +71,7 @@ def train_transforms_pidnet(conf_augmentation):
     return TC.Compose(preprocess, additional_targets={'edge': 'mask'})
 
 
-def val_transforms_pidnet(conf_augmentation):
-    assert conf_augmentation.img_size > 32
-    preprocess = [
-        TC.Resize((conf_augmentation.img_size, conf_augmentation.img_size), interpolation='bilinear', max_size=None),
-        TC.ToTensor(),
-        TC.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD)
-    ]
-    return TC.Compose(preprocess, additional_targets={'edge': 'mask'})
-
-
 def create_transform(model_name: str, is_training=False):
     if 'pidnet' in model_name:
-        return train_transforms_pidnet if is_training else val_transforms_pidnet
+        return partial(train_transforms_pidnet, training=is_training)
     return partial(transforms_custom, training=is_training)
