6d50405bec0f7fe56ff4651539dc4ada6d2889b7
Junho Shin <junho.shin@nota.ai>
2024-02-19 08:20:36 +0000

    Update configs

--- src/netspresso_trainer/cfg/training.py
+++ src/netspresso_trainer/cfg/training.py
@@ -5,10 +5,29 @@ from omegaconf import MISSING, MissingMandatoryValue
 
 
 @dataclass
+class EMA:
+    name: str = MISSING
+    decay: float = MISSING
+
+
+@dataclass
+class ConstantDecayEMA(EMA):
+    name: str = 'constant_decay'
+    decay: float = 0.9999
+
+
+@dataclass
+class ExpDecayEMA(EMA):
+    name: str = 'exp_decay'
+    decay: float = 0.9999
+    beta: float = 15
+
+
+@dataclass
 class ScheduleConfig:
     epochs: int = 3
     batch_size: int = 8
-    ema_decay: Optional[float] = None
+    ema: Optional[EMA] = None
     optimizer: Dict = field(default_factory=lambda: {
         "name": "adamw",
         "lr": 6e-5,
