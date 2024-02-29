771261dfbdf418053ec5d4f376a54187fa381b54
junho.shin <junho.shin@nota.ai>
2024-02-29 07:24:11 +0000

    Define train_step

--- src/netspresso_trainer/pipelines/pose_estimation.py
+++ src/netspresso_trainer/pipelines/pose_estimation.py
@@ -14,7 +14,32 @@ class PoseEstimationPipeline(BasePipeline):
                                                 train_dataloader, eval_dataloader, class_map, logging_dir, **kwargs)
 
     def train_step(self, batch):
-        pass
+        self.model.train()
+        images, keypoints = batch['pixel_values'], batch['label']
+        images = images.to(self.devices)
+        target = {'keypoints': keypoints.to(self.devices)}
+
+        self.optimizer.zero_grad()
+
+        out = self.model(images)
+        self.loss_factory.calc(out, target, phase='train')
+
+        self.loss_factory.backward()
+        self.optimizer.step()
+
+        pred = self.postprocessor(out)
+
+        if self.conf.distributed:
+            gathered_pred = [None for _ in range(torch.distributed.get_world_size())]
+            gathered_labels = [None for _ in range(torch.distributed.get_world_size())]
+
+            torch.distributed.gather_object(pred, gathered_pred if torch.distributed.get_rank() == 0 else None, dst=0)
+            torch.distributed.gather_object(keypoints, gathered_labels if torch.distributed.get_rank() == 0 else None, dst=0)
+            torch.distributed.barrier()
+            if torch.distributed.get_rank() == 0:
+                [self.metric_factory.calc(g_pred, g_labels, phase='train') for g_pred, g_labels in zip(gathered_pred, gathered_labels)]
+        else:
+            self.metric_factory.calc(pred, keypoints, phase='train')
 
     def valid_step(self, eval_model, batch):
         pass
