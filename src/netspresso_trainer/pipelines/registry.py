163943aa65593b92b744b212c604348cd63c7c68
junho.shin <junho.shin@nota.ai>
2024-03-21 08:52:57 +0000

    Move taskpipelines

--- src/netspresso_trainer/pipelines/registry.py
+++ src/netspresso_trainer/pipelines/registry.py
@@ -1,10 +1,10 @@
 from typing import Dict, List, Type
 
-from .base import BasePipeline
-from .classification import ClassificationPipeline
-from .detection import DetectionPipeline
-from .pose_estimation import PoseEstimationPipeline
-from .segmentation import SegmentationPipeline
+from .task_pipelines.base import BasePipeline
+from .task_pipelines.classification import ClassificationPipeline
+from .task_pipelines.detection import DetectionPipeline
+from .task_pipelines.pose_estimation import PoseEstimationPipeline
+from .task_pipelines.segmentation import SegmentationPipeline
 
 # TODO: Temporary defined. It should be integrated with `..models.registry.SUPPORTING_TASK_LIST`
 SUPPORTING_TASK_LIST: List[str] = ['classification', 'segmentation', 'detection', 'pose_estimation']
