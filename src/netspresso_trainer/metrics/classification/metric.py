18b4137ad15728af1ebf024eacdb195f2a112c76
junho.shin <junho.shin@nota.ai>
2024-04-04 07:40:22 +0000

    Fix ClassificationMetric to update metric in cabrate method

--- src/netspresso_trainer/metrics/classification/metric.py
+++ src/netspresso_trainer/metrics/classification/metric.py
@@ -16,20 +16,25 @@ def accuracy_topk(pred, target):
     pred = pred.T
     class_num = pred.shape[0]
     correct = np.equal(pred, np.tile(target, (class_num, 1)))
-    return lambda topk: correct[:min(topk, maxk)].reshape(-1).astype('float').sum(0) * 100. / batch_size
+    return lambda topk: correct[:min(topk, maxk)].reshape(-1).astype('float').sum(0)
 
 
 class ClassificationMetric(BaseMetric):
-    metric_names: List[str] = ['Acc@1', 'Acc@5']
-    primary_metric: str = 'Acc@1'
+    SUPPORT_METRICS: List[str] = ['Acc@1', 'Acc@5']
 
     def __init__(self, **kwargs):
-        super().__init__()
+        # TODO: Select metrics by user
+        metric_names = ['Acc@1', 'Acc@5']
+        primary_metric = 'Acc@1'
+
+        assert set(metric_names).issubset(ClassificationMetric.SUPPORT_METRICS)
+        super().__init__(metric_names=metric_names, primary_metric=primary_metric)
 
     def calibrate(self, pred, target, **kwargs):
-        result_dict = {k: 0. for k in self.metric_names}
         topk_callable = accuracy_topk(pred, target)
 
-        result_dict['Acc@1'] = topk_callable(topk=1)
-        result_dict['Acc@5'] = topk_callable(topk=5)
-        return result_dict
+        Acc1_correct = topk_callable(topk=1)
+        Acc5_correct = topk_callable(topk=5)
+
+        self.metric_meter['Acc@1'].update(Acc1_correct, n=pred.shape[0])
+        self.metric_meter['Acc@5'].update(Acc5_correct, n=pred.shape[0])
