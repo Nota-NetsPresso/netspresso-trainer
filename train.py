8e49a3d07ff9cd8b68ff9e810579904d1751b44d
deepkyu <hyoungkyu.song@nota.ai>
2024-04-05 13:34:10 +0900

    revert train code

--- train.py
+++ train.py
@@ -48,10 +48,10 @@ def train_with_inline_yaml():
     from netspresso_trainer import train_with_yaml
     logging_dir = train_with_yaml(
         # gpus="0,1",
-        data="config/data/sidewalk-semantic.yaml",
-        augmentation="config/augmentation/segmentation.yaml",
-        model="config/model/pidnet/pidnet-s-segmentation.yaml",
-        training="config/training/segmentation.yaml",
+        data="config/data/beans.yaml",
+        augmentation="config/augmentation/classification.yaml",
+        model="config/model/resnet/resnet50-classification.yaml",
+        training="config/training/classification.yaml",
         logging="config/logging.yaml",
         environment="config/environment.yaml",
         log_level='INFO'
@@ -63,9 +63,9 @@ if __name__ == '__main__':
     # logging_dir = train_cli()
 
     # With inline yaml
-    logging_dir = train_with_inline_yaml()
+    # logging_dir = train_with_inline_yaml()
     
     # With inline pythonic config
-    # logging_dir = train_with_inline_cfg()
+    logging_dir = train_with_inline_cfg()
     
     print(f"Training results are saved at: {logging_dir}")
