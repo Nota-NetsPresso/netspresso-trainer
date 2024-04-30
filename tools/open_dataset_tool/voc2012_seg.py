f60a1fca1cd2adfcaa4d528b182a68372f1c3beb
junho.shin <junho.shin@nota.ai>
2024-04-30 09:50:36 +0000

    Add label image mode and change id_mapping to json

--- tools/open_dataset_tool/voc2012_seg.py
+++ tools/open_dataset_tool/voc2012_seg.py
@@ -3,39 +3,41 @@ from pathlib import Path
 import os
 import tarfile
 import shutil
+import json
 
 import cv2
 import numpy as np
 import pandas as pd
 import torch
 from tqdm import tqdm
+from PIL import Image
 
 DEFAULT_DATA_DIR = './data'
 DOWNLOAD_DIR = './data/download'
 VOC2012_URL = 'http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar'
 VOC2012_ID_MAPPING_RGB = {
-    (0, 0, 0): 'background',
-    (128, 0, 0): 'aeroplane',
-    (0, 128, 0): 'bicycle',
-    (128, 128, 0): 'bird',
-    (0, 0, 128): 'boat',
-    (128, 0, 128): 'bottle',
-    (0, 128, 128): 'bus',
-    (128, 128, 128): 'car',
-    (64, 0, 0): 'cat',
-    (192, 0, 0): 'chair',
-    (64, 128, 0): 'cow',
-    (192, 128, 0): 'diningtable',
-    (64, 0, 128): 'dog',
-    (192, 0, 128): 'horse',
-    (64, 128, 128): 'motorbike',
-    (192, 128, 128): 'person',
-    (0, 64, 0): 'pottedplant',
-    (128, 64, 0): 'sheep',
-    (0, 192, 0): 'sofa',
-    (128, 192, 0): 'train',
-    (0, 64, 128): 'tvmonitor',
-    (128, 64, 128): 'void',
+    '(0, 0, 0)': 'background',
+    '(128, 0, 0)': 'aeroplane',
+    '(0, 128, 0)': 'bicycle',
+    '(128, 128, 0)': 'bird',
+    '(0, 0, 128)': 'boat',
+    '(128, 0, 128)': 'bottle',
+    '(0, 128, 128)': 'bus',
+    '(128, 128, 128)': 'car',
+    '(64, 0, 0)': 'cat',
+    '(192, 0, 0)': 'chair',
+    '(64, 128, 0)': 'cow',
+    '(192, 128, 0)': 'diningtable',
+    '(64, 0, 128)': 'dog',
+    '(192, 0, 128)': 'horse',
+    '(64, 128, 128)': 'motorbike',
+    '(192, 128, 128)': 'person',
+    '(0, 64, 0)': 'pottedplant',
+    '(128, 64, 0)': 'sheep',
+    '(0, 192, 0)': 'sofa',
+    '(128, 192, 0)': 'train',
+    '(0, 64, 128)': 'tvmonitor',
+    '(128, 64, 128)': 'void',
 }
 
 
@@ -50,6 +52,7 @@ if __name__ == '__main__':
     # Set argument (data directory)
     parser = argparse.ArgumentParser(description="Parser for VOC 2012 dataset downloader.")
     parser.add_argument('--dir', type=str, default=DEFAULT_DATA_DIR)
+    parser.add_argument('--label_image_mode', type=str, default='P', choices=['RGB', 'P', 'L'], help='Label image mode (RGB or P or L)')
     args = parser.parse_args()
 
     # Download VOC 2012 dataset
@@ -59,10 +62,10 @@ if __name__ == '__main__':
         print(f'Download path {download_path} already exists! download step is skipped.')
     else:
         torch.hub.download_url_to_file(VOC2012_URL, download_path)
-    
+
     # Extract tar file
     ap = tarfile.open(download_path)
-  
+
     voc2012_path = Path(args.dir) / 'voc2012_seg'
     os.makedirs(voc2012_path, exist_ok=True)
     ap.extractall(voc2012_path)
@@ -80,13 +83,19 @@ if __name__ == '__main__':
     # Move train images and masks
     train_image_dir = voc2012_path / 'images' / 'train'
     train_label_dir = voc2012_path / 'labels' / 'train'
-    
+
     os.makedirs(train_image_dir, exist_ok=True)
     os.makedirs(train_label_dir, exist_ok=True)
-    for sample in train_samples:
+    for sample in tqdm(train_samples):
         shutil.move(img_src / (sample + '.jpg'), train_image_dir / (sample + '.jpg'))
-        shutil.move(label_src / (sample + '.png'), train_label_dir / (sample + '.png'))
-    
+        if args.label_image_mode == 'L':
+            label = Image.open(label_src / (sample + '.png'))
+            label = np.array(label)
+            label = Image.fromarray(label, mode='L')
+            label.save(train_label_dir / (sample + '.png'))
+        else:
+            Image.open(label_src / (sample + '.png')).save(train_label_dir / (sample + '.png'))
+
     # Get valid sample list
     valid_samples = voc2012_path / 'VOCdevkit' / 'VOC2012' / 'ImageSets' / 'Segmentation' / 'val.txt'
     with open(valid_samples, 'r') as f:
@@ -99,18 +108,24 @@ if __name__ == '__main__':
 
     os.makedirs(valid_image_dir, exist_ok=True)
     os.makedirs(valid_label_dir, exist_ok=True)
-    for sample in valid_samples:
+    for sample in tqdm(valid_samples):
         shutil.move(img_src / (sample + '.jpg'), valid_image_dir / (sample + '.jpg'))
-        shutil.move(label_src / (sample + '.png'), valid_label_dir / (sample + '.png'))
-    
+        if args.label_image_mode == 'L':
+            label = Image.open(label_src / (sample + '.png'))
+            label = np.array(label)
+            label = Image.fromarray(label, mode='L')
+            label.save(valid_label_dir / (sample + '.png'))
+        else:
+            Image.open(label_src / (sample + '.png')).save(valid_label_dir / (sample + '.png'))
+
     # Build id_mapping
-    text = ''
-    for color, label in VOC2012_ID_MAPPING_RGB.items():
-        text += f'{color}:{label}\n'
-    
-    with open(voc2012_path / 'rgb_id_mapping.txt', 'w') as f:
-        f.write(text)
-        f.close()
+    if args.label_image_mode == 'L':
+        id_mapping_to_save = list(VOC2012_ID_MAPPING_RGB.values())
+    else:
+        id_mapping_to_save = VOC2012_ID_MAPPING_RGB
+
+    with open(voc2012_path / 'id_mapping.json', 'w') as f:
+        json.dump(id_mapping_to_save, f)
 
     try:
         shutil.rmtree(voc2012_path / 'VOCdevkit')
