eeb6ab4666d9f93bf5239857abb86eb7b7292240
junho.shin <junho.shin@nota.ai>
2024-05-07 01:44:01 +0000

    Remove thop

--- src/netspresso_trainer/utils/stats.py
+++ src/netspresso_trainer/utils/stats.py
@@ -1,7 +1,6 @@
 import logging
 from typing import TypedDict
 
-import thop
 import torch
 import torch.nn as nn
 from fvcore.nn import FlopCountAnalysis
@@ -14,9 +13,6 @@ def get_params_and_macs(model: nn.Module, sample_input: torch.Tensor):
     # From v0.0.9
     macs, params = _params_and_macs_fvcore(model, sample_input)
 
-    # # Before v0.0.9
-    # macs, params = _params_and_macs_thop(model, sample_input)
-
     return macs, params
 
 def _params_and_macs_fvcore(model: nn.Module, sample_input: torch.Tensor):
@@ -24,8 +20,4 @@ def _params_and_macs_fvcore(model: nn.Module, sample_input: torch.Tensor):
     fvcore_logger.setLevel(logging.CRITICAL)
     macs = FlopCountAnalysis(model, sample_input).total()
     params = sum(p.numel() for p in model.parameters())
-    return macs, params
-
-def _params_and_macs_thop(model: nn.Module, sample_input: torch.Tensor):
-    macs, params = thop.profile(model, inputs=(sample_input,), verbose=False)
     return macs, params
