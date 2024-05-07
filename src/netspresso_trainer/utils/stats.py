a361eab357452b9577c07769eaafbcd0de6695d1
junho.shin <junho.shin@nota.ai>
2024-05-07 01:48:37 +0000

    Change MACs to FLOPs

--- src/netspresso_trainer/utils/stats.py
+++ src/netspresso_trainer/utils/stats.py
@@ -11,13 +11,13 @@ from .environment import get_device
 def get_params_and_macs(model: nn.Module, sample_input: torch.Tensor):
     sample_input = sample_input.to(get_device(model))
     # From v0.0.9
-    macs, params = _params_and_macs_fvcore(model, sample_input)
+    flops, params = _params_and_macs_fvcore(model, sample_input)
 
-    return macs, params
+    return flops, params
 
 def _params_and_macs_fvcore(model: nn.Module, sample_input: torch.Tensor):
     fvcore_logger = logging.getLogger('fvcore')
     fvcore_logger.setLevel(logging.CRITICAL)
-    macs = FlopCountAnalysis(model, sample_input).total()
+    flops = FlopCountAnalysis(model, sample_input).total()
     params = sum(p.numel() for p in model.parameters())
-    return macs, params
+    return flops, params
