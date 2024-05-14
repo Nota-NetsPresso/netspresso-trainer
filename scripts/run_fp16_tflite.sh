b47c6c6dfd79bb9e80e7502f7655bcf90b3b60ab
Junho Shin <junho.shin@nota.ai>
2024-05-14 18:22:21 +0900

    Lower score thresh

--- scripts/run_fp16_tflite.sh
+++ scripts/run_fp16_tflite.sh
@@ -1,5 +1,5 @@
 python ./device_demo/demo.py\
     --model-path ./detection_yolox_s_graphmodule_epoch_450.tflite\
     --input-data 0\
-    --score-thresh 0.6\
+    --score-thresh 0.3\
     --nms-thresh 0.65
