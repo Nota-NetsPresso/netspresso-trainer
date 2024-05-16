2fc908c1089e8f1b84edbe7007e09308dbd7ca50
Junho Shin <junho.shin@nota.ai>
2024-05-16 11:58:07 +0900

    I was using wrong model...

--- scripts/run_original_tflite.sh
+++ scripts/run_original_tflite.sh
@@ -1,5 +1,5 @@
 python ./device_demo/demo.py\
-    --model-path ./detection_yolox_s_graphmodule_epoch_450.tflite\
+    --model-path ./detection_yolox_s_epoch_426.tflite\
     --model-type original\
     --input-data 0\
     --score-thresh 0.4\
