b28ec55766cfaedcc029baaf05b1ed31fbfdfe38
Junho Shin <junho.shin@nota.ai>
2024-05-16 11:55:19 +0900

    Fix typo

--- scripts/run_nota_tflite.sh
+++ scripts/run_nota_tflite.sh
@@ -1,6 +1,6 @@
 python ./device_demo/demo.py\
     --model-path detection_yolox_s_graphmodule_epoch_450_head_modified_int8.tflite\
-    --model-type cocmpressed\
+    --model-type compressed\
     --input-data 0\
     --score-thresh 0.4\
     --nms-thresh 0.65
