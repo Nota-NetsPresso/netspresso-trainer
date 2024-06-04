a02bd0b1b57252af0370612b47b76ee795554e81
junho.shin <junho.shin@nota.ai>
2024-06-04 05:23:01 +0000

    Mis-updated

--- scripts/run_nota_tflite.sh
+++
@@ -1,6 +0,0 @@
-python ./device_demo/demo.py\
-    --model-path detection_yolox_s_graphmodule_epoch_450_head_modified_int8.tflite\
-    --model-type compressed\
-    --input-data 0\
-    --score-thresh 0.4\
-    --nms-thresh 0.65
