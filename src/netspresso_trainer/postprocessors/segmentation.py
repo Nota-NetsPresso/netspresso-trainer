0a3ca15e6f6cfa80e2351fe013ef41c19a51fe82
hglee98 <hyungeun.lee@nota.ai>
2024-06-17 13:51:43 +0900

    init segmentation postprocessor params

--- src/netspresso_trainer/postprocessors/segmentation.py
+++ src/netspresso_trainer/postprocessors/segmentation.py
@@ -24,8 +24,7 @@ from ..models.utils import ModelOutput
 
 class SegmentationPostprocessor:
     def __init__(self, conf_model):
-        pass
-
+        self.params = conf_model.postprocessor.params        
     def __call__(self, outputs: ModelOutput, original_shape):
         pred = outputs['pred']
         H, W = original_shape[-2:]
