39044538184e4c9694d32777aa165f735e212e7e
Junho Shin <junho.shin@nota.ai>
2024-08-12 05:02:04 +0000

    Change func name get_params_and_macs to get_params_and_flops

--- src/netspresso_trainer/utils/stats.py
+++ src/netspresso_trainer/utils/stats.py
@@ -24,7 +24,7 @@ from fvcore.nn import FlopCountAnalysis
 from .environment import get_device
 
 
-def get_params_and_macs(model: nn.Module, sample_input: torch.Tensor):
+def get_params_and_flops(model: nn.Module, sample_input: torch.Tensor):
     sample_input = sample_input.to(get_device(model))
     # From v0.0.9
     flops, params = _params_and_macs_fvcore(model, sample_input)
