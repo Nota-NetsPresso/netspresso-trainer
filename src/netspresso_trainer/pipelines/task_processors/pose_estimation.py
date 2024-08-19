cc9e94e6822f504b3e3a53e87245fecf7701ef3d
hglee98 <hyungeun.lee@nota.ai>
2024-08-19 05:50:17 +0000

    [feat] Add gradient clipping to poseEstimation pipeline

--- src/netspresso_trainer/pipelines/task_processors/pose_estimation.py
+++ src/netspresso_trainer/pipelines/task_processors/pose_estimation.py
@@ -14,7 +14,7 @@
 #
 # ----------------------------------------------------------------------------
 
-from typing import Literal
+from typing import Literal, Optional
 
 import numpy as np
 import torch
@@ -26,7 +26,7 @@ class PoseEstimationProcessor(BaseTaskProcessor):
     def __init__(self, conf, postprocessor, devices, **kwargs):
         super(PoseEstimationProcessor, self).__init__(conf, postprocessor, devices, **kwargs)
 
-    def train_step(self, train_model, batch, optimizer, loss_factory, metric_factory):
+    def train_step(self, train_model, batch, optimizer, loss_factory, metric_factory, model_max_norm: Optional[float]=None):
         train_model.train()
         images, keypoints = batch['pixel_values'], batch['keypoints']
         images = images.to(self.devices)
@@ -39,6 +39,11 @@ class PoseEstimationProcessor(BaseTaskProcessor):
             loss_factory.calc(out, target, phase='train')
 
         loss_factory.backward(self.grad_scaler)
+        if model_max_norm:
+            # Unscales the gradients of optimizer's assigned parameters in-place
+            self.grad_scaler.unscale_(optimizer)
+            torch.nn.utils.clip_grad_norm_(train_model.parameters(), model_max_norm)
+        # optimizer's gradients are already unscaled, so scaler.step doesn't unscale them,
         self.grad_scaler.step(optimizer)
         self.grad_scaler.update()
 
