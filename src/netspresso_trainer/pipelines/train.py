6f77b03cebf639e5bd0207b8fca3ccead5e53496
hglee98 <hyungeun.lee@nota.ai>
2024-08-22 07:26:56 +0000

    [refactor] delete `model_max_norm` argument from `train_step` method

--- src/netspresso_trainer/pipelines/train.py
+++ src/netspresso_trainer/pipelines/train.py
@@ -189,7 +189,7 @@ class TrainingPipeline(BasePipeline):
     def train_one_epoch(self, epoch):
         outputs = []
         for _idx, batch in enumerate(tqdm(self.train_dataloader, leave=False)):
-            out = self.task_processor.train_step(self.model, batch, self.optimizer, self.loss_factory, self.metric_factory, self.model_max_norm)
+            out = self.task_processor.train_step(self.model, batch, self.optimizer, self.loss_factory, self.metric_factory)
             if self.model_ema:
                 self.model_ema.update(model=self.model.module if hasattr(self.model, 'module') else self.model)
             outputs.append(out)
