8fc2411184585338f98b3e1535d7a31980278bb1
Junho Shin <junho.shin@nota.ai>
2024-08-23 10:00:31 +0000

    Add hard-coded mobilenetv4

--- src/netspresso_trainer/models/backbones/experimental/mobilenetv4.py
+++ src/netspresso_trainer/models/backbones/experimental/mobilenetv4.py
@@ -25,7 +25,7 @@ import torch
 import torch.nn as nn
 from torch import Tensor
 
-from ...op.custom import ConvLayer, InvertedResidual
+from ...op.custom import ConvLayer, UniversalInvertedBottleneckBlock
 from ...utils import BackboneOutput
 from ..registry import USE_INTERMEDIATE_FEATURES_TASK_LIST
 
@@ -49,11 +49,66 @@ class MobileNetV4(nn.Module):
 
         super(MobileNetV4, self).__init__()
 
+        # Implement on conv-small
+        norm_type = 'batch_norm'
+        act_type = 'relu'
+
+        self.conv_stem = ConvLayer(3, 32, kernel_size=3, stride=2, padding=1, bias=False, norm_type=norm_type, act_type=act_type)
+
+        stages = []
+
+        # TODO: Replace with for loop
+        stage1 = [
+            ConvLayer(32, 32, kernel_size=3, stride=2, padding=1, bias=False, norm_type=norm_type, act_type=act_type),
+            ConvLayer(32, 32, kernel_size=1, stride=1, padding=1, bias=False, norm_type=norm_type, act_type=act_type),
+        ]
+
+        stage2 = [
+            ConvLayer(32, 96, kernel_size=3, stride=2, padding=1, bias=False, norm_type=norm_type, act_type=act_type),
+            ConvLayer(96, 64, kernel_size=1, stride=1, padding=1, bias=False, norm_type=norm_type, act_type=act_type),
+        ]
+
+        stage3 = [
+            UniversalInvertedBottleneckBlock(64, 192, 96, True, 5, True, 5, 2, norm_type, act_type),
+            UniversalInvertedBottleneckBlock(96, 192, 96, False, None, True, 3, 1, norm_type, act_type),
+            UniversalInvertedBottleneckBlock(96, 192, 96, False, None, True, 3, 1, norm_type, act_type),
+            UniversalInvertedBottleneckBlock(96, 192, 96, False, None, True, 3, 1, norm_type, act_type),
+            UniversalInvertedBottleneckBlock(96, 192, 96, False, None, True, 3, 1, norm_type, act_type),
+            UniversalInvertedBottleneckBlock(96, 384, 96, True, 3, False, None, 1, norm_type, act_type),
+        ]
+
+        stage4 = [
+            UniversalInvertedBottleneckBlock(96, 576, 128, True, 3, True, 3, 2, norm_type, act_type),
+            UniversalInvertedBottleneckBlock(128, 512, 128, True, 5, True, 5, 1, norm_type, act_type),
+            UniversalInvertedBottleneckBlock(128, 512, 128, False, None, True, 5, 1, norm_type, act_type),
+            UniversalInvertedBottleneckBlock(128, 384, 128, False, None, True, 5, 1, norm_type, act_type),
+            UniversalInvertedBottleneckBlock(128, 512, 128, False, None, True, 3, 1, norm_type, act_type),
+            UniversalInvertedBottleneckBlock(128, 512, 128, False, None, True, 3, 1, norm_type, act_type),
+        ]
+
+        stages = [stage1, stage2, stage3, stage4]
+
+        # Add conv on last stage
+        stages[-1].append(ConvLayer(128, 960, kernel_size=1, stride=1, bias=False, norm_type=norm_type, act_type=act_type))
+
+        # Build stages
+        stages = [nn.Sequential(*stage) for stage in stages]
+        self.stages = nn.ModuleList(stages)
+
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self._feature_dim = 960
+
         # Dummy
-        self._feature_dim = 1024
-        self._intermediate_features_dim = [256, 512, 1024]
+        self._intermediate_features_dim = [64, 96, 960]
 
     def forward(self, x: Tensor):
+        x = self.conv_stem(x)
+        for stage in self.stages:
+            x = stage(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+
         return BackboneOutput(last_feature=x)
 
     @property
