72fbc336262e6cbf07b6a7e07b419f87d95727c2
Junho Shin <junho.shin@nota.ai>
2024-09-09 09:09:05 +0000

    Add overwrite and no_weight_decay option for optimizer

--- src/netspresso_trainer/optimizers/builder.py
+++ src/netspresso_trainer/optimizers/builder.py
@@ -18,18 +18,124 @@ from typing import Literal
 
 import torch.nn as nn
 from omegaconf import DictConfig
+from loguru import logger
 
 from .registry import OPTIMIZER_DICT
 
 
+def build_overwrited_dict(optimizer_conf):
+    tmp_optimizer_conf = optimizer_conf.copy()
+    del tmp_optimizer_conf['overwrite'] # Remove unnecessary field
+    overwrited_config_dict = {
+        'backbone': tmp_optimizer_conf.copy(),
+        'neck': tmp_optimizer_conf.copy(),
+        'head': tmp_optimizer_conf.copy(),
+    }
+
+    overwrite_config = optimizer_conf.overwrite
+    for module_overwrite_config in overwrite_config:
+        if not hasattr(module_overwrite_config, 'param_group'):
+            raise ValueError(f"Overwrite config must have 'param_group' field: {module_overwrite_config}")
+
+        param_group = module_overwrite_config.param_group
+
+        if hasattr(module_overwrite_config, 'lr'):
+            overwrited_config_dict[param_group].lr = module_overwrite_config.lr
+        if hasattr(module_overwrite_config, 'weight_decay'):
+            overwrited_config_dict[param_group].weight_decay = module_overwrite_config.weight_decay
+        if hasattr(module_overwrite_config, 'no_bias_weight_decay'):
+            overwrited_config_dict[param_group].no_bias_weight_decay = module_overwrite_config.no_bias_weight_decay
+        if hasattr(module_overwrite_config, 'no_norm_weight_decay'):
+            overwrited_config_dict[param_group].no_norm_weight_decay = module_overwrite_config.no_norm_weight_decay
+
+    return overwrited_config_dict
+
+
+def split_param_groups(model, overwrited_config_dict):
+    backbone_params = set()
+    neck_params = set()
+    head_params = set()
+
+    # Separate parameters by module
+    named_params_dict = dict(model.named_parameters())
+    for k in named_params_dict.keys():
+        if k.startswith('backbone'):
+            backbone_params.add(k)
+        elif k.startswith('neck'):
+            neck_params.add(k)
+        elif k.startswith('head'):
+            head_params.add(k)
+        else:
+            raise ValueError(f"Unknown parameter group: {k}") # Only for NetsPresso Trainer defined model
+
+    # Separate parameters by weight, bias, and norm
+    def separate_no_weights_decay(params: set, module_config: DictConfig):
+        bias_params = set(filter(lambda s: 'bias' in s, params))
+        norm_params = set(filter(lambda s: 'norm.weight' in s, params))
+        no_decay_params = set()
+        if module_config.no_bias_weight_decay:
+            no_decay_params |= bias_params
+            params -= bias_params
+        if module_config.no_norm_weight_decay:
+            no_decay_params |= norm_params
+            params -= norm_params
+        return params, no_decay_params
+
+    backbone_params = separate_no_weights_decay(backbone_params, overwrited_config_dict['backbone'])
+    neck_params = separate_no_weights_decay(neck_params, overwrited_config_dict['neck'])
+    head_params = separate_no_weights_decay(head_params, overwrited_config_dict['head'])
+
+    param_set_dict = {'backbone': backbone_params, 'neck': neck_params, 'head': head_params}
+
+    # Build param_groups and param_configs
+    param_groups = []
+    param_opt_configs = []
+    for param_group_key, module_overwrite_config in overwrited_config_dict.items():
+        # Remove unnecessary fields
+        del module_overwrite_config['no_bias_weight_decay']
+        del module_overwrite_config['no_norm_weight_decay']
+
+        params, no_decay_params = param_set_dict[param_group_key]
+
+        module_overwrite_config.group_name = param_group_key + '_group'
+        if len(params) != 0: # Skip if no parameters, e.g. no neck in the model
+            param_groups.append([named_params_dict[k] for k in params]) # Convert from key to parameters
+            param_opt_configs.append(module_overwrite_config.copy())
+
+        module_overwrite_config.group_name = param_group_key + '_no_decay_group'
+        module_overwrite_config.weight_decay = 0.0
+        if len(no_decay_params) != 0: # Skip if no parameters, e.g. use weight decay for all parameters
+            param_groups.append([named_params_dict[k] for k in no_decay_params])
+            param_opt_configs.append(module_overwrite_config.copy())
+
+    return param_groups, param_opt_configs
+
 def build_optimizer(
-    model_or_params,
+    model,
+    single_task_model: bool,
     optimizer_conf: DictConfig,
 ):
-    parameters = model_or_params.parameters() if isinstance(model_or_params, nn.Module) else model_or_params
+    no_dist_model = model.module if hasattr(model, 'module') else model
 
     opt_name: Literal['sgd', 'adam', 'adamw', 'adamax', 'adadelta', 'adagrad', 'rmsprop'] = optimizer_conf.name.lower()
     assert opt_name in OPTIMIZER_DICT
 
-    optimizer = OPTIMIZER_DICT[opt_name](parameters, optimizer_conf)
+    overwrite_config = optimizer_conf.overwrite
+    if overwrite_config:
+        assert not single_task_model, "Overwrite config only for non single task model"
+        overwrited_config_dict = build_overwrited_dict(optimizer_conf)
+        param_groups, param_opt_configs = split_param_groups(no_dist_model, overwrited_config_dict)
+
+        base_param_group = param_groups.pop(0)
+        base_opt_config = param_opt_configs.pop(0)
+        optimizer = OPTIMIZER_DICT[opt_name](base_param_group, base_opt_config)
+        optimizer.param_groups[0]['group_name'] = base_opt_config.group_name # Add group name manually
+
+        for param_group, opt_config in zip(param_groups, param_opt_configs):
+            del opt_config['name'] # Remove unnecessary field
+            optimizer.add_param_group({'params': param_group, **opt_config})
+
+    else:
+        optimizer = OPTIMIZER_DICT[opt_name](no_dist_model.parameters(), optimizer_conf)
+
     return optimizer
