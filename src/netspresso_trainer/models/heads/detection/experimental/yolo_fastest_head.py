f26e2edb2eadd4abe2ca34ef523ce435acec7360
hglee98 <hyungeun.lee@nota.ai>
2024-09-30 08:16:05 +0000

    [refactor] Update yolo-fastest-head

--- src/netspresso_trainer/models/heads/detection/experimental/yolo_fastest_head.py
+++ src/netspresso_trainer/models/heads/detection/experimental/yolo_fastest_head.py
@@ -33,18 +33,18 @@ class YOLOFastestHeadV2(nn.Module):
         params: DictConfig) -> None:
         super().__init__()
         anchors = params.anchors
-        num_anchors = len(anchors[0]) // 2
-        in_channel = intermediate_features_dim[0]
-        self.cls_head = YOLOFastestClassificationHead(in_channel, num_anchors, num_classes)  
-        self.reg_head = YOLOFastestRegressionHead(in_channel, num_anchors) 
+        self.num_anchors = len(anchors[0]) // 2
+        hidden_dim = int(intermediate_features_dim[0])
+        self.cls_head = YOLOFastestClassificationHead(hidden_dim, self.num_anchors, num_classes)  
+        self.reg_head = YOLOFastestRegressionHead(hidden_dim, self.num_anchors) 
 
     def forward(self, x, target=None):
         cls_logits, objs = self.cls_head(x)
         bbox_regression = self.reg_head(x)
         outputs = list()
         for reg, obj, logits in zip(bbox_regression, objs, cls_logits):
-            pred = torch.cat([reg, obj, logits], dim=-1)
-            outputs.append(pred)
+            output = torch.cat([reg, obj, logits], 1)
+            outputs.append(output)
         return ModelOutput(pred=outputs)
 
 def yolo_fastest_head_v2(num_classes, intermediate_features_dim, conf_model_head) -> YOLOFastestHeadV2:
@@ -59,88 +59,77 @@ class YOLOFastestClassificationHead(nn.Module):
         in_channels,
         num_anchors, 
         num_classes,
-        prior_prob = 0.01, 
+        prior_prob = 1e-2,
+        num_layers = 2,
         ) -> None:
         super().__init__()
-
-        self.layer_1 = nn.Sequential(*[
-            SeparableConvLayer(in_channels, in_channels, 5, padding=2, no_out_act=True),
-            SeparableConvLayer(in_channels, in_channels, 5, padding=2, no_out_act=True),
-        ])
-        self.layer_2 = nn.Sequential(*[
-            SeparableConvLayer(in_channels, in_channels, 5, padding=2, no_out_act=True),
-            SeparableConvLayer(in_channels, in_channels, 5, padding=2, no_out_act=True),
-        ])
-        
-        self.cls_logits = nn.Conv2d(in_channels, num_classes * num_anchors, 1, 1, 0, bias=True)
-        self.obj = nn.Conv2d(in_channels, num_anchors, 1, 1, 0, bias=True) 
-        nn.init.normal_(self.cls_logits.weight, std=0.01)
-        nn.init.constant_(self.cls_logits.bias, -math.log((1 - prior_prob) / prior_prob))
-
         self.num_classes = num_classes 
         self.num_anchors = num_anchors
+        self.layer = nn.ModuleList()
+        self.cls_logits = nn.ModuleList()
+        self.obj = nn.ModuleList()
+        for _ in range(num_layers):
+            self.layer.append(
+                nn.Sequential(*[
+                    SeparableConvLayer(in_channels, in_channels, 5, padding=2, no_out_act=True),
+                    SeparableConvLayer(in_channels, in_channels, 5, padding=2, no_out_act=True),
+                ])
+            )
+            self.cls_logits.append(nn.Conv2d(in_channels, num_classes * num_anchors, 1, 1, 0, bias=True))
+            self.obj.append(nn.Conv2d(in_channels, num_anchors, 1, 1, 0, bias=True))
+        
+        self.initialize_biases(prior_prob=prior_prob)
+        
+    
+    def initialize_biases(self, prior_prob):
+        for conv in self.cls_logits:
+            b = conv.bias.view(1, -1)
+            b.data.fill_(-math.log((1 - prior_prob) / prior_prob))
+            conv.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
+
+        for conv in self.obj:
+            b = conv.bias.view(1, -1)
+            b.data.fill_(-math.log((1 - prior_prob) / prior_prob))
+            conv.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
     
     def forward(self, x): 
         all_cls_logits = [] 
         all_objs = []
-        out1 = self.layer_1(x[0])
-        out2 = self.layer_2(x[1])
-        outputs = [out1, out2]
-
-        for features in outputs: 
+        for idx, out in enumerate(x):
+            features = self.layer[idx](out)
             cls_logits = features
-            objectness = cls_logits
-            cls_logits = self.cls_logits(cls_logits)
-            objectness = self.obj(objectness)
-            
-            # Permute classification output from (N, A * K, H, W) to (N, H, W, A, K).
-            N, _, H, W = cls_logits.shape
-            cls_logits = cls_logits.view(N, -1, self.num_classes, H, W)
-            cls_logits = cls_logits.permute(0, 3, 4, 1, 2)
+            objectness = features
+            cls_logits = self.cls_logits[idx](cls_logits)
+            objectness = self.obj[idx](objectness)
             all_cls_logits.append(cls_logits)
-
-            # Permute objectness output from (N, A, H, W) to (N, H, W, A, K).
-            N, _, H, W = objectness.shape
-            objectness = objectness.view(N, -1, 1, H, W)
-            objectness = objectness.permute(0, 3, 4, 1, 2)
             all_objs.append(objectness)
-        
         return all_cls_logits, all_objs
-
 
 
 class YOLOFastestRegressionHead(nn.Module): 
     def __init__(
         self,
         in_channels, 
-        num_anchors,) -> None:
+        num_anchors,
+        num_layers=2,
+        ) -> None:
         super().__init__()
-
-        self.layer_1 = nn.Sequential(*[
-            SeparableConvLayer(in_channels, in_channels, 5, padding=2, no_out_act=True),
-            SeparableConvLayer(in_channels, in_channels, 5, padding=2, no_out_act=True),
-        ])
-        self.layer_2 = nn.Sequential(*[
-            SeparableConvLayer(in_channels, in_channels, 5, padding=2, no_out_act=True),
-            SeparableConvLayer(in_channels, in_channels, 5, padding=2, no_out_act=True),
-        ])
-        self.bbox_reg = nn.Conv2d(in_channels, num_anchors * 4, 1, 1, 0, bias=True)
+        self.layer = nn.ModuleList()
+        self.bbox_reg = nn.ModuleList()
+        for _ in range(num_layers):
+            self.layer.append(
+                nn.Sequential(*[
+                                SeparableConvLayer(in_channels, in_channels, 5, padding=2, no_out_act=True),
+                                SeparableConvLayer(in_channels, in_channels, 5, padding=2, no_out_act=True)
+                                ]))
+            self.bbox_reg.append(nn.Conv2d(in_channels, 4 * num_anchors, 1, 1, 0))
     
-    def forward(self, x, targets=None): 
+    def forward(self, x): 
         all_bbox_regression = []
-        out1 = self.layer_1(x[0])
-        out2 = self.layer_2(x[1])
-        outputs = [out1, out2]
-
-        for features in outputs: 
+        for idx, out in enumerate(x):
+            features = self.layer[idx](out)
             bbox_regression = features
-            bbox_regression = self.bbox_reg(bbox_regression)
-
-            # Permute bbox regression output from (N, 4 * A, H, W) to (N, HWA, 4).
-            N, _, H, W = bbox_regression.shape
-            bbox_regression = bbox_regression.view(N, -1, 4, H, W)
-            bbox_regression = bbox_regression.permute(0, 3, 4, 1, 2)
+            bbox_regression = self.bbox_reg[idx](bbox_regression)
             all_bbox_regression.append(bbox_regression)
-        
         return all_bbox_regression
 
