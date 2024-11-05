73072bd5c9e6fe0e13dc9abcb837b59280ab872d
hglee98 <hyungeun.lee@nota.ai>
2024-11-05 08:31:06 +0000

    [refactor] Add `POOL_REGISTRY`

--- src/netspresso_trainer/models/op/registry.py
+++ src/netspresso_trainer/models/op/registry.py
@@ -34,3 +34,8 @@ ACTIVATION_REGISTRY: Dict[str, Type[nn.Module]] = {
     'swish': nn.SiLU,
     'hard_swish': nn.Hardswish,
 }
+
+POOL_RESGISTRY: Dict[str, Type[nn.Module]] = {
+    'max': nn.MaxPool2d,
+    'avg': nn.AvgPool2d,
+}
