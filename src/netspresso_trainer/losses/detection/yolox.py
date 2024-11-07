cc6514107d3e7d97023bc32bc295b142bafd609f
hglee98 <hyungeun.lee@nota.ai>
2024-11-07 05:11:17 +0000

    [style] ruff fix

--- src/netspresso_trainer/losses/detection/yolox.py
+++ src/netspresso_trainer/losses/detection/yolox.py
@@ -20,7 +20,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .utils import xyxy2cxcywh, bboxes_iou
+from .utils import bboxes_iou, xyxy2cxcywh
 
 
 class YOLOXLoss(nn.Module):
