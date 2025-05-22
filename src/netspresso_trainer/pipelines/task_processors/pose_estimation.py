df06f98b373d1209913c048445bc686376bfd001
Junho Shin <junho.shin@nota.ai>
2025-05-22 18:12:53 +0900

    refactor pose processor

--- src/netspresso_trainer/pipelines/task_processors/pose_estimation.py
+++ src/netspresso_trainer/pipelines/task_processors/pose_estimation.py
@@ -22,6 +22,7 @@ import torch
 from netspresso_trainer.models.utils import set_training_targets
 
 from .base import BaseTaskProcessor
+from ...utils.protocols import ProcessorStepOut
 
 
 class PoseEstimationProcessor(BaseTaskProcessor):
@@ -31,6 +32,8 @@ class PoseEstimationProcessor(BaseTaskProcessor):
     def train_step(self, train_model, batch, optimizer, loss_factory, metric_factory):
         train_model.train()
         images, keypoints = batch['pixel_values'], batch['keypoints']
+        images = torch.stack(images, dim=0)
+        keypoints = torch.stack(keypoints, dim=0)
         images = images.to(self.devices)
         target = {'keypoints': keypoints.to(self.devices)}
 
@@ -64,16 +67,20 @@ class PoseEstimationProcessor(BaseTaskProcessor):
                 pred = np.concatenate(gathered_pred, axis=0)
                 keypoints = np.concatenate(gathered_labels, axis=0)
 
+        step_out = ProcessorStepOut.empty()
         if self.single_gpu_or_rank_zero:
-            logs = {
-                'target': keypoints,
-                'pred': pred
-            }
-            return dict(logs.items())
+            step_out['pred'] = list(pred)
+            step_out['target'] = list(keypoints)
+
+        return step_out
 
     def valid_step(self, eval_model, batch, loss_factory, metric_factory):
         eval_model.eval()
+        name = batch['name']
         indices, images, keypoints = batch['indices'], batch['pixel_values'], batch['keypoints']
+        indices = torch.stack(indices, dim=0)
+        images = torch.stack(images, dim=0)
+        keypoints = torch.stack(keypoints, dim=0)
         images = images.to(self.devices)
         target = {'keypoints': keypoints.to(self.devices)}
 
@@ -85,30 +92,37 @@ class PoseEstimationProcessor(BaseTaskProcessor):
         indices = indices.numpy()
         keypoints = keypoints.detach().cpu().numpy()
         if self.conf.distributed:
+            name = np.array(name)[indices != -1].tolist()
             pred = pred[indices != -1]
             keypoints = keypoints[indices != -1]
 
+            gathered_name = [None for _ in range(torch.distributed.get_world_size())]
             gathered_pred = [None for _ in range(torch.distributed.get_world_size())]
             gathered_labels = [None for _ in range(torch.distributed.get_world_size())]
 
+            torch.distributed.gather_object(name, gathered_name if torch.distributed.get_rank() == 0 else None, dst=0)
             torch.distributed.gather_object(pred, gathered_pred if torch.distributed.get_rank() == 0 else None, dst=0)
             torch.distributed.gather_object(keypoints, gathered_labels if torch.distributed.get_rank() == 0 else None, dst=0)
             torch.distributed.barrier()
             if torch.distributed.get_rank() == 0:
+                name = sum(gathered_name, [])
                 pred = np.concatenate(gathered_pred, axis=0)
                 keypoints = np.concatenate(gathered_labels, axis=0)
 
+        step_out = ProcessorStepOut.empty()
         if self.single_gpu_or_rank_zero:
-            logs = {
-                'images': images.detach().cpu().numpy(),
-                'target': keypoints,
-                'pred': pred
-            }
-            return dict(logs.items())
+            step_out['name'] = name
+            step_out['pred'] = list(pred)
+            step_out['target'] = list(keypoints)
+
+        return step_out
 
     def test_step(self, test_model, batch):
         test_model.eval()
+        name = batch['name']
         indices, images = batch['indices'], batch['pixel_values']
+        indices = torch.stack(indices, dim=0)
+        images = torch.stack(images, dim=0)
         images = images.to(self.devices)
 
         out = test_model(images)
@@ -117,19 +131,26 @@ class PoseEstimationProcessor(BaseTaskProcessor):
 
         indices = indices.numpy()
         if self.conf.distributed:
+            name = np.array(name)[indices != -1].tolist()
             pred = pred[indices != -1]
 
+            gathered_name = [None for _ in range(torch.distributed.get_world_size())]
             gathered_pred = [None for _ in range(torch.distributed.get_world_size())]
 
+            torch.distributed.gather_object(name, gathered_name if torch.distributed.get_rank() == 0 else None, dst=0)
             torch.distributed.gather_object(pred, gathered_pred if torch.distributed.get_rank() == 0 else None, dst=0)
             torch.distributed.barrier()
             if torch.distributed.get_rank() == 0:
                 gathered_pred = sum(gathered_pred, [])
+                name = sum(gathered_name, [])
                 pred = gathered_pred
 
+        step_out = ProcessorStepOut.empty()
         if self.single_gpu_or_rank_zero:
-            results = {'images': images.detach().cpu().numpy(), 'pred': pred}
-            return results
+            step_out['name'] = name
+            step_out['pred'] = list(pred)
+
+        return step_out
 
     def get_metric_with_all_outputs(self, outputs, phase: Literal['train', 'valid'], metric_factory):
         if self.single_gpu_or_rank_zero:
